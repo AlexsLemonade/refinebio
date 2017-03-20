f1dc38e45f774dc84ac17de49128143c68c6985e
Kurt Wheeler <kurt.wheeler91@gmail.com>
2017-03-20 09:54:14 -0400

    Dockerfiles now install data models locally instead of from pip.

--- data_models/bioinformatics_mill_models/models.py
+++ data_models/bioinformatics_mill_models/models.py
@@ -21,35 +21,41 @@ class SurveyJob(TimeTrackedModel):
     source_type = models.CharField(max_length=256)
 
     # The start time of the query used to replicate
-    replication_started_at = models.DateTimeField()
+    replication_started_at = models.DateTimeField(null=True)
 
     # The end time of the query used to replicate
-    replication_ended_at = models.DateTimeField()
+    replication_ended_at = models.DateTimeField(null=True)
 
     # The start time of the job
-    start_time = models.DateTimeField()
+    start_time = models.DateTimeField(null=True)
 
     # The end time of the job
-    end_time = models.DateTimeField()
+    end_time = models.DateTimeField(null=True)
 
     class Meta:
         db_table = "survey_jobs"
 
 
-# This model is still has a prototypical status, but I needed something to
-# test with and it's at least in the right ballpark
+class BatchStatuses(Enum):
+    NEW = "NEW"
+    DOWNLOADING = "DOWNLOADING"
+    DOWNLOADED = "DOWNLOADED"
+    PROCESSING = "PROCESSING"
+    PROCESSED = "PROCESSED"
+
+
 class Batch(TimeTrackedModel):
     survey_job = models.ForeignKey(SurveyJob)
     source_type = models.CharField(max_length=256)
     size_in_bytes = models.IntegerField()
     download_url = models.CharField(max_length=256)
-    raw_format = models.CharField(max_length=256)
-    processed_format = models.CharField(max_length=256)
+    raw_format = models.CharField(max_length=256, null=True)
+    processed_format = models.CharField(max_length=256, null=True)
     pipeline_required = models.CharField(max_length=256)
     accession_code = models.CharField(max_length=32)
 
     # This field where denote where in our system the file can be found
-    internal_location = models.CharField(max_length=256)
+    internal_location = models.CharField(max_length=256, null=True)
 
     # This will at some point be a meaningful integer->organism lookup thing
     organism = models.IntegerField()
@@ -61,7 +67,7 @@ class Batch(TimeTrackedModel):
         ("PROCESSING", "Processing"),
         ("PROCESSED", "Proccessed"),
     )
-    status = models.CharField(max_length=10, choices=STATUSES)
+    status = models.CharField(max_length=10)
 
     class Meta:
         db_table = "batches"
@@ -83,10 +89,10 @@ class BatchKeyValue(TimeTrackedModel):
 class ProcessorJob(TimeTrackedModel):
     batch_id = models.ForeignKey(Batch, on_delete=models.CASCADE)
     pipeline_applied = models.CharField(max_length=256)
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
-    success = models.NullBooleanField()
-    num_retries = models.IntegerField()
+    start_time = models.DateTimeField(null=True)
+    end_time = models.DateTimeField(null=True)
+    success = models.NullBooleanField(null=True)
+    num_retries = models.IntegerField(default=0)
 
     # This point of this field is to identify what worker ran the job.
     # A few fields may actually be required or something other than just an id.
@@ -98,10 +104,10 @@ class ProcessorJob(TimeTrackedModel):
 
 class DownloaderJob(TimeTrackedModel):
     batch_id = models.ForeignKey(Batch, on_delete=models.CASCADE)
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
-    success = models.NullBooleanField()
-    num_retries = models.IntegerField()
+    start_time = models.DateTimeField(null=True)
+    end_time = models.DateTimeField(null=True)
+    success = models.NullBooleanField(null=True)
+    num_retries = models.IntegerField(default=0)
     worker_id = models.CharField(max_length=256)
 
     class Meta:
--- workers/downloaders/tasks.py
+++ workers/downloaders/tasks.py
@@ -1,23 +1,34 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 
-from bioinformatics_mill_models.models import Batch
+from bioinformatics_mill_models.models import Batch, BatchStatuses, SurveyJob
 
 
 @shared_task
 def save_test_batch():
+    print("Saving survey job")
+    test_survey_job = SurveyJob(
+        source_type="ARRAY_EXPRESS",
+        replication_started_at="2001-04-10 15:51:24Z",
+        replication_ended_at="2001-04-10 15:51:24Z",
+        start_time="2001-04-10 15:51:24Z",
+        end_time="2001-04-10 15:51:24Z",
+    )
+    test_survey_job.save()
+
     print("saving batch")
     test_batch = Batch(
+        survey_job=test_survey_job,
         source_type="ARRAY_EXPRESS",
         size_in_bytes=1000,
         download_url="www.example.com",
         raw_format="MICRO_ARRAY",
         processed_format="PCL",
-        processor_required=1,
+        pipeline_required="MICRO_ARRAY_TO_PCL",
         accession_code="E-MEXP-123",
         internal_location="expression_data/array_express/E-MEXP-123",
         organism=1,
-        status="PROCESSED"
+        status=BatchStatuses.NEW
     )
 
     test_batch.save()
