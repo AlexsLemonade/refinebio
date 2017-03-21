e8a78b8f1fdb7da3d0a328353ef71ff17bbef475
Kurt Wheeler <kurt.wheeler91@gmail.com>
2017-03-21 13:26:50 -0400

    Leave some sources for information on network settings.

--- data_models/install.sh
+++ data_models/install.sh
@@ -13,8 +13,13 @@ psql data_refinery -c "CREATE ROLE data_refinery_user WITH LOGIN PASSWORD 'data_
 psql data_refinery -c 'GRANT ALL PRIVILEGES ON DATABASE data_refinery TO data_refinery_user;'
 psql data_refinery -c 'ALTER USER data_refinery_user CREATEDB;'
 
+# See https://www.howtogeek.com/177621/the-beginners-guide-to-iptables-the-linux-firewall/
+# for more information.
 iptables -A INPUT -s 172.17.0.0/16 -j ACCEPT
 
+# See http://stackoverflow.com/questions/31249112/allow-docker-container-to-connect-to-a-local-host-postgres-database
+# and https://blog.jsinh.in/how-to-enable-remote-access-to-postgresql-database-server/#.WNFiBCErLmG
+# for more information on these two settings.
 echo 'host    all             all             172.17.0.0/16           md5' >> /etc/postgresql/$POST_GRES_VERSION/main/pg_hba.conf
 
 sed -i "s/#\(listen_addresses\)\( = 'localhost'\)/\1 = '*'/" /etc/postgresql/$POSTGRES_VESRION/main/postgresql.conf
