df02113cabedec13b0b40d21d9fd768fab09b88b
Kurt Wheeler <kurt.wheeler91@gmail.com>
2017-03-30 13:33:43 -0400

    Removed some now unnecessary comments.

--- foreman/data_refinery_foreman/surveyor/surveyor.py
+++ foreman/data_refinery_foreman/surveyor/surveyor.py
@@ -13,8 +13,6 @@ def _get_surveyor_for_source(survey_job: SurveyJob):
     if(survey_job.source_type == "ARRAY_EXPRESS"):
         return ArrayExpressSurveyor(survey_job)
     else:
-        # Probably should be more specific, but I'm not sure what exception to
-        # use yet
         raise SourceNotSupportedError(
             "Source " + source_type + " is not supported.")
 
@@ -40,7 +38,6 @@ def run_job(survey_job: SurveyJob):
 
     try:
         surveyor = _get_surveyor_for_source(survey_job)
-    # once again, should be more specific
     except SourceNotSupportedError as e:
         # This should be logging, not printing. I need to set that up.
         log_message = "Unable to run survey job # " + survey_job.id
