63fb77b222c92e9619611c70416c2a9c5b466994
Kurt Wheeler <kurt.wheeler91@gmail.com>
2017-04-10 14:54:54 -0400

    Working on getting the downloaders moved to the right spot.

--- workers/data_refinery_workers/master.py
+++ workers/data_refinery_workers/master.py
@@ -1,4 +1,4 @@
-from downloaders.tasks import save_test_batch
+from data_refinery_workers.downloaders.tasks import save_test_batch
 
 # This is just a temporary way to test calling a task of the Celery worker.
 
