9dafe76c2f9eef64280188f8e8a0ce1b1426108c
Kurt Wheeler <kurt.wheeler91@gmail.com>
2017-04-11 16:44:42 -0400

    Some cleanup and clarification based on PR feedback.

--- data_models/install.sh
+++ data_models/install.sh
@@ -13,10 +13,15 @@ psql data_refinery -c "CREATE ROLE data_refinery_user WITH LOGIN PASSWORD 'data_
 psql data_refinery -c 'GRANT ALL PRIVILEGES ON DATABASE data_refinery TO data_refinery_user;'
 psql data_refinery -c 'ALTER USER data_refinery_user CREATEDB;'
 
-# See https://www.howtogeek.com/177621/the-beginners-guide-to-iptables-the-linux-firewall/
+# See http://unix.stackexchange.com/questions/52376/why-do-iptables-rules-disappear-when-restarting-my-debian-system
 # for more information.
+# However upon restarting you'll either need to rerun:
 iptables -A INPUT -s 172.17.0.0/16 -j ACCEPT
+
+# Or take advantage of the fact that this has been run:
 /sbin/iptables-save > /etc/iptables/rules
+# To run:
+# iptables-restore < /etc/iptables.conf
 
 
 # See http://stackoverflow.com/questions/31249112/allow-docker-container-to-connect-to-a-local-host-postgres-database
--- data_models/run_tests.sh
+++ data_models/run_tests.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-# script for executing Django Unittests within Docker container.
+# script for executing Django PyUnit Tests within a Docker container.
+
+# This script should always run as if it were being called from
+# the directory it lives in.
+cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 docker build -t models_tests -f Dockerfile.tests .
 
 HOST_IP=$(ifconfig eth0 | grep "inet " | awk -F'[: ]+' '{ print $4 }')
 docker run \
        --add-host=database:$HOST_IP \
-       --env-file /home/kurt/Development/data_refinery/data_models/environments/test \
+       --env-file environments/test \
        -i models_tests "$@"
