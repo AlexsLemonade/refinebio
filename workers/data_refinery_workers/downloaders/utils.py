b4757a34172e0daefe729da717767cc40102037c
Kurt Wheeler <kurt.wheeler91@gmail.com>
2017-04-12 11:43:36 -0400

    Actually add the utils file I promised and add volume to gitignore.

---
+++ workers/data_refinery_workers/downloaders/utils.py
@@ -0,0 +1,17 @@
+from django.utils import timezone
+from data_refinery_models.models import Batch, DownloaderJob
+
+
+def start_job(job: DownloaderJob):
+    """Record in the database that this job is being started. """
+    job.worker_id = "For now there's only one. For now..."
+    job.start_time = timezone.now()
+    job.save()
+
+
+def end_job(job: DownloaderJob, batch: Batch, success):
+    """Record in the database that this job has completed.
+    This should also queue a processor job at some point."""
+    job.success = success
+    job.end_time = timezone.now()
+    job.save()
