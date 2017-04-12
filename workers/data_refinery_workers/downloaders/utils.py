a84b0f8dbee508f5fbcc6400be427d015bded3fd
Kurt Wheeler <kurt.wheeler91@gmail.com>
2017-04-12 16:09:46 -0400

    Factor out the filepath stuff into utils.

--- workers/data_refinery_workers/downloaders/utils.py
+++ workers/data_refinery_workers/downloaders/utils.py
@@ -1,5 +1,10 @@
+import os
 from django.utils import timezone
 from data_refinery_models.models import Batch, BatchStatuses, DownloaderJob
+
+
+# This path is within the Docker container.
+ROOT_URI = "/home/user/data_store/raw/"
 
 
 def start_job(job: DownloaderJob):
@@ -18,3 +23,13 @@ def end_job(job: DownloaderJob, batch: Batch, success):
 
     batch.status = BatchStatuses.DOWNLOADED.value
     batch.save()
+
+
+def prepare_destination(batch: Batch):
+    """Prepare the destination directory and return the full
+    path the Batch's file should be downloaded to."""
+    target_directory = ROOT_URI + batch.internal_location
+    os.makedirs(target_directory, exist_ok=True)
+
+    filename = batch.download_url.split('/')[-1]
+    return target_directory + filename
