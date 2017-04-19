acc3d299274d8c53b08193bffb1c2878efd8a3d3
Kurt Wheeler <kurt.wheeler91@gmail.com>
2017-04-19 15:55:27 -0400

    Deletes now unneeded files due to dependencies being managed through conda.

--- workers/conda_install.sh
+++
@@ -1,9 +0,0 @@
-bash miniconda.sh -b -p $HOME/miniconda
-PATH="$HOME/miniconda/bin:$PATH"
-
-conda install -c bioconda bioconductor-affy=1.50.0
-conda install -c bioconda bioconductor-affyio=1.42.0
-
-# rpy2 only uses miniconda/lib/R/lib, not miniconda/lib
-# Since we can't change what rpy2 uses, just move the lib files.
-cp miniconda/lib/lib* miniconda/lib/R/lib
