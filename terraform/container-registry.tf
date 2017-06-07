177e9947c55f6ffa25c6fed379d5eadaa3a5a14f
Kurt Wheeler <kurt.wheeler91@gmail.com>
2017-06-07 11:53:44 -0400

    Fixes health checks stopping container instances.

--- terraform/container-registry.tf
+++ terraform/container-registry.tf
@@ -60,12 +60,5 @@ resource "aws_ecs_service" "data_refinery_worker" {
   desired_count  = 2
   deployment_minimum_healthy_percent = 50
   deployment_maximum_percent = 200
-  iam_role = "${aws_iam_role.ecs_service_role.name}"
   depends_on = ["aws_iam_role_policy.ecs_service"]
-
-  load_balancer {
-    elb_name = "${aws_elb.data_refinery_worker.name}"
-    container_name = "data-refinery-worker"
-    container_port = 8000
-  }
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -187,38 +187,3 @@ resource "aws_route_table_association" "data_refinery_1b" {
   subnet_id      = "${aws_subnet.data_refinery_1b.id}"
   route_table_id = "${aws_route_table.data_refinery.id}"
 }
-
-resource "aws_elb" "data_refinery_worker" {
-  name = "data-refinery-worker"
-  subnets = [
-    "${aws_subnet.data_refinery_1a.id}",
-    "${aws_subnet.data_refinery_1b.id}"]
-
-  listener {
-    instance_port = 8000
-    instance_protocol = "http"
-    lb_port = 80
-    lb_protocol = "http"
-  }
-
-  health_check {
-    healthy_threshold = 10
-    unhealthy_threshold = 2
-    timeout = 5
-    target = "TCP:8000"
-    interval = 30
-  }
-
-  security_groups = ["${aws_security_group.data_refinery_worker.id}"]
-
-  instances = [
-    "${aws_instance.data_refinery_worker_1.id}",
-    "${aws_instance.data_refinery_worker_2.id}",
-  ]
-
-  cross_zone_load_balancing = true
-  idle_timeout = 60
-  connection_draining = true
-  connection_draining_timeout = 400
-  internal = true
-}
