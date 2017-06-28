959e7495fe866233756a1dd44ed308f1cc047334
Kurt Wheeler <kurt.wheeler91@gmail.com>
2017-06-28 15:53:33 -0400

    Fix manual intervention for migrations.

--- data_models/make_migrations.sh
+++ data_models/make_migrations.sh
@@ -16,12 +16,11 @@ docker run \
        --volume $script_directory/data_refinery_models:/home/user/data_refinery_models \
        --add-host=database:$HOST_IP \
        --env-file environments/dev \
-       --detach\
+       --interactive \
        dr_models makemigrations data_refinery_models
 
 docker run \
        --volume $script_directory/data_refinery_models:/home/user/data_refinery_models \
        --add-host=database:$HOST_IP \
        --env-file environments/dev \
-       --detach \
        dr_models
