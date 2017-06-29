2ace73e3fdf93dacb51ef94c08386ab78949290c
Kurt Wheeler <kurt.wheeler91@gmail.com>
2017-06-29 16:08:11 -0400

    Adds database timeouts for tests so CI tests fail faster.

--- .circleci/wait_for_postgres.sh
+++ .circleci/wait_for_postgres.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-while [ 0 -ne $(PGPASSWORD=data_refinery_password psql -d data_refinery -U data_refinery_user -h 127.0.0.1 -c 'select 1;' 2>&1 | grep -c '^ERROR') ]; do
+while [ 0 -ne $(PGPASSWORD=data_refinery_password psql \
+                          -d data_refinery \
+                          -U data_refinery_user \
+                          -h 127.0.0.1 \
+                          -c 'select 1;' 2>&1 \
+                    | grep -c '^ERROR') ]; do
     sleep 1
 done
