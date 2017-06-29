d28e201c1903dc5beab3b994cdfaf41564ad982f
Kurt Wheeler <kurt.wheeler91@gmail.com>
2017-06-29 15:57:29 -0400

    Adds script to wait until postgres database is online.

---
+++ .circleci/wait_for_postgres.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+while [ 0 -ne $(PGPASSWORD=data_refinery_password psql -d data_refinery -U data_refinery_user -h 127.0.0.1 -c 'select 1;' 2>&1 | grep -c '^ERROR') ]; do
+    sleep 1
+done
