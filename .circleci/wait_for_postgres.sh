9f35b7d9607629ced48eae789f916c38f179cabc
Kurt Wheeler <kurt.wheeler91@gmail.com>
2017-06-30 16:19:49 -0400

    Trying out the machine executor.

--- .circleci/wait_for_postgres.sh
+++ .circleci/wait_for_postgres.sh
@@ -6,5 +6,6 @@ while [ 0 -ne $(PGPASSWORD=data_refinery_password psql \
                           -h 127.0.0.1 \
                           -c 'select 1;' 2>&1 \
                     | grep -c '^ERROR') ]; do
+    echo "Postgres isn't alive yet."
     sleep 1
 done
