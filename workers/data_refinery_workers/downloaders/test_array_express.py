793c570cfa0df773e1508da25df07b6a2f835ee5
Kurt Wheeler <kurt.wheeler91@gmail.com>
2017-08-10 11:01:49 -0400

    Improves tests for array express downloader.

--- workers/data_refinery_workers/downloaders/test_array_express.py
+++ workers/data_refinery_workers/downloaders/test_array_express.py
@@ -1,4 +1,5 @@
 import copy
+from typing import List
 from unittest.mock import patch, MagicMock, call
 from django.test import TestCase
 from data_refinery_models.models import (
@@ -13,43 +14,15 @@ from data_refinery_common import file_management
 
 
 class DownloadArrayExpressTestCase(TestCase):
-    def test_good_batch_grouping(self):
-        """Returns true if all batches have the same download_url."""
-        batches = [Batch(download_url="https://example.com"),
-                   Batch(download_url="https://example.com"),
-                   Batch(download_url="https://example.com")]
-        job_id = 1
-        self.assertIsNone(array_express._verify_batch_grouping(batches, job_id))
-
-    def test_bad_batch_grouping(self):
-        """Raises exception if all batches don't have the same download_url."""
-        batches = [Batch(download_url="https://example.com"),
-                   Batch(download_url="https://example.com"),
-                   Batch(download_url="https://wompwomp.com")]
-        job_id = 1
-        with self.assertRaises(ValueError):
-            array_express._verify_batch_grouping(batches, job_id)
-
-    @patch("data_refinery_workers.downloaders.utils.app")
-    @patch("data_refinery_workers.downloaders.array_express._verify_batch_grouping")
-    @patch("data_refinery_workers.downloaders.array_express._download_file")
-    @patch("data_refinery_workers.downloaders.array_express._extract_file")
-    def test_download(self,
-                      _extract_file,
-                      _download_file,
-                      _verify_batch_grouping,
-                      app):
-        # Set up mocks:
-        app.send_task = MagicMock()
-        app.send_task.return_value = None
-
-        # Set up database records:
+    def setUp(self):
         survey_job = SurveyJob(source_type="ARRAY_EXPRESS")
         survey_job.save()
+        self.survey_job = survey_job
 
+    def insert_batches(self) -> List[Batch]:
         download_url = "ftp://ftp.ebi.ac.uk/pub/databases/microarray/data/experiment/GEOD/E-GEOD-59071/E-GEOD-59071.raw.3.zip"  # noqa
         batch = Batch(
-            survey_job=survey_job,
+            survey_job=self.survey_job,
             source_type="ARRAY_EXPRESS",
             size_in_bytes=0,
             download_url=download_url,
@@ -71,8 +44,42 @@ class DownloadArrayExpressTestCase(TestCase):
         batch2.name = "CE2345.CEL"
         batch.save()
         batch2.save()
+        return [batch, batch2]
 
-        downloader_job = DownloaderJob.create_job_and_relationships(batches=[batch, batch2])
+    def test_good_batch_grouping(self):
+        """Returns true if all batches have the same download_url."""
+        batches = self.insert_batches()
+        downloader_job = DownloaderJob.create_job_and_relationships(
+            batches=batches, downloader_task="dummy")
+
+        self.assertIsNone(array_express._verify_batch_grouping(batches, downloader_job))
+
+    def test_bad_batch_grouping(self):
+        """Raises exception if all batches don't have the same download_url."""
+        batches = self.insert_batches()
+        batches[1].download_url = "https://wompwomp.com"
+        batches[1].save()
+        downloader_job = DownloaderJob.create_job_and_relationships(
+            batches=batches, downloader_task="dummy")
+
+        with self.assertRaises(ValueError):
+            array_express._verify_batch_grouping(batches, downloader_job)
+
+    @patch("data_refinery_workers.downloaders.utils.app")
+    @patch("data_refinery_workers.downloaders.array_express._verify_batch_grouping")
+    @patch("data_refinery_workers.downloaders.array_express._download_file")
+    @patch("data_refinery_workers.downloaders.array_express._extract_file")
+    def test_download(self,
+                      _extract_file,
+                      _download_file,
+                      _verify_batch_grouping,
+                      app):
+        # Set up mocks:
+        app.send_task = MagicMock()
+        app.send_task.return_value = None
+
+        batches = self.insert_batches()
+        downloader_job = DownloaderJob.create_job_and_relationships(batches=batches)
 
         # Call the task we're testing:
         array_express.download_array_express.apply(args=(downloader_job.id,)).get()
@@ -82,10 +89,11 @@ class DownloadArrayExpressTestCase(TestCase):
 
         # Verify that all expected functionality is run:
         self.assertEqual(_verify_batch_grouping.call_count, 1)
+        download_url = "ftp://ftp.ebi.ac.uk/pub/databases/microarray/data/experiment/GEOD/E-GEOD-59071/E-GEOD-59071.raw.3.zip"  # noqa
         _download_file.assert_called_with(download_url, target_file_path, downloader_job)
         args, _ = _extract_file.call_args
         batch_query_set, job = args
-        self.assertEqual(list(batch_query_set), [batch, batch2])
+        self.assertEqual(list(batch_query_set), batches)
         self.assertEqual(job.id, downloader_job.id)
 
         # Verify that the database has been updated correctly:
@@ -119,41 +127,13 @@ class DownloadArrayExpressTestCase(TestCase):
         app.send_task = MagicMock()
         app.send_task.return_value = None
 
-        # Set up database records:
-        survey_job = SurveyJob(source_type="ARRAY_EXPRESS")
-        survey_job.save()
-
-        download_url = "ftp://ftp.ebi.ac.uk/pub/databases/microarray/data/experiment/GEOD/E-GEOD-59071/E-GEOD-59071.raw.3.zip"  # noqa
-        batch = Batch(
-            survey_job=survey_job,
-            source_type="ARRAY_EXPRESS",
-            size_in_bytes=0,
-            download_url=download_url,
-            raw_format="CEL",
-            processed_format="PCL",
-            pipeline_required="AFFY_TO_PCL",
-            platform_accession_code="A-AFFY-1",
-            experiment_accession_code="E-MTAB-3050",
-            experiment_title="It doesn't really matter.",
-            name="CE1234.CEL",
-            internal_location="A-AFFY-1/AFFY_TO_PCL/",
-            organism_id=9606,
-            organism_name="HOMO SAPIENS",
-            release_date="2017-05-05",
-            last_uploaded_date="2017-05-05",
-            status=BatchStatuses.NEW.value
-        )
-
         # Set a different download URL to trigger a failure in the
         # _verify_batch_grouping function
         different_download_url = "ftp://ftp.ebi.ac.uk/pub/databases/microarray/data/experiment/GEOD/E-GEOD-59071/E-GEOD-59070.raw.2.zip"  # noqa
-        batch2 = copy.deepcopy(batch)
-        batch2.name = "CE2345.CEL"
-        batch2.download_url = different_download_url
-        batch.save()
-        batch2.save()
-
-        downloader_job = DownloaderJob.create_job_and_relationships(batches=[batch, batch2])
+        batches = self.insert_batches()
+        batches[1].download_url = different_download_url
+        batches[1].save()
+        downloader_job = DownloaderJob.create_job_and_relationships(batches=batches)
 
         # Call the download task
         array_express.download_array_express.apply(args=(downloader_job.id,)).get()
@@ -182,36 +162,8 @@ class DownloadArrayExpressTestCase(TestCase):
         app.send_task.return_value = None
         _open.side_effect = Exception()
 
-        # Set up database records:
-        survey_job = SurveyJob(source_type="ARRAY_EXPRESS")
-        survey_job.save()
-
-        download_url = "ftp://ftp.ebi.ac.uk/pub/databases/microarray/data/experiment/GEOD/E-GEOD-59071/E-GEOD-59071.raw.3.zip"  # noqa
-        batch = Batch(
-            survey_job=survey_job,
-            source_type="ARRAY_EXPRESS",
-            size_in_bytes=0,
-            download_url=download_url,
-            raw_format="CEL",
-            processed_format="PCL",
-            pipeline_required="AFFY_TO_PCL",
-            platform_accession_code="A-AFFY-1",
-            experiment_accession_code="E-MTAB-3050",
-            experiment_title="It doesn't really matter.",
-            name="CE1234.CEL",
-            internal_location="A-AFFY-1/AFFY_TO_PCL/",
-            organism_id=9606,
-            organism_name="HOMO SAPIENS",
-            release_date="2017-05-05",
-            last_uploaded_date="2017-05-05",
-            status=BatchStatuses.NEW.value
-        )
-        batch2 = copy.deepcopy(batch)
-        batch2.name = "CE2345.CEL"
-        batch.save()
-        batch2.save()
-
-        downloader_job = DownloaderJob.create_job_and_relationships(batches=[batch, batch2])
+        batches = self.insert_batches()
+        downloader_job = DownloaderJob.create_job_and_relationships(batches=batches)
 
         # Call the download task
         array_express.download_array_express.apply(args=(downloader_job.id,)).get()
@@ -236,36 +188,8 @@ class DownloadArrayExpressTestCase(TestCase):
         app.send_task = MagicMock()
         app.send_task.return_value = None
 
-        # Set up database records:
-        survey_job = SurveyJob(source_type="ARRAY_EXPRESS")
-        survey_job.save()
-
-        download_url = "ftp://ftp.ebi.ac.uk/pub/databases/microarray/data/experiment/GEOD/E-GEOD-59071/E-GEOD-59071.raw.3.zip"  # noqa
-        batch = Batch(
-            survey_job=survey_job,
-            source_type="ARRAY_EXPRESS",
-            size_in_bytes=0,
-            download_url=download_url,
-            raw_format="CEL",
-            processed_format="PCL",
-            pipeline_required="AFFY_TO_PCL",
-            platform_accession_code="A-AFFY-1",
-            experiment_accession_code="E-MTAB-3050",
-            experiment_title="It doesn't really matter.",
-            name="CE1234.CEL",
-            internal_location="A-AFFY-1/AFFY_TO_PCL/",
-            organism_id=9606,
-            organism_name="HOMO SAPIENS",
-            release_date="2017-05-05",
-            last_uploaded_date="2017-05-05",
-            status=BatchStatuses.NEW.value
-        )
-        batch2 = copy.deepcopy(batch)
-        batch2.name = "CE2345.CEL"
-        batch.save()
-        batch2.save()
-
-        downloader_job = DownloaderJob.create_job_and_relationships(batches=[batch, batch2])
+        batches = self.insert_batches()
+        downloader_job = DownloaderJob.create_job_and_relationships(batches=batches)
 
         # Call the download task
         array_express.download_array_express.apply(args=(downloader_job.id,)).get()
@@ -279,6 +203,6 @@ class DownloadArrayExpressTestCase(TestCase):
         self.assertIsNotNone(downloader_job.end_time)
 
         job_dir = array_express.JOB_DIR_PREFIX + str(downloader_job.id)
-        zip_path = file_management.get_temp_download_path(batch, job_dir)
+        zip_path = file_management.get_temp_download_path(batches[0], job_dir)
         self.assertEqual(downloader_job.failure_reason,
                          "Exception caught while extracting " + zip_path)
