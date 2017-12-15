e520a0f4804613063d3d68f4ce66f0bbf2ae813c
Kurt Wheeler <kurt.wheeler91@gmail.com>
2017-12-15 10:59:03 -0500

    Polishes up run_nomad.sh to make it work a non-hacky way.

--- run_nomad.sh
+++ run_nomad.sh
@@ -3,10 +3,28 @@
 HOST_IP=$(ip route get 8.8.8.8 | awk '{print $NF; exit}')
 
 # Start the nomad in development locally
-nomad agent -bind $HOST_IP -data-dir /home/kurt/Development/data_refinery/nomad_dir -dev > nomad.logs &
+nomad agent -bind $HOST_IP \
+      -data-dir /home/kurt/Development/data_refinery/nomad_dir \
+      -dev \
+      > nomad.logs &
 
-# Hacky way to wait for Nomad to get started
-sleep 5
+echo "Waiting for Nomad to come online."
+
+check_nomad_status () {
+    echo $(curl --write-out %{http_code} \
+                  --silent \
+                  --output /dev/null \
+                  http://$HOST_IP:4646/v1/status/leader)
+}
+
+# Wait for Nomad to get started.
+nomad_status=$(check_nomad_status)
+while [ $nomad_status != "200" ]; do
+    sleep 0.5
+    nomad_status=$(check_nomad_status)
+done
+
+echo "Nomad is online, registering jobs."
 
 # Register the jobs for dispatching.
 nomad run -address http://$HOST_IP:4646 downloader.nomad
