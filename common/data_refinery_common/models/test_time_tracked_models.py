051dcc59b27974cf5ee8c18214b16a7ffa0461c2
Kurt Wheeler <kurt.wheeler91@gmail.com>
2017-12-18 14:46:50 -0500

    Removes references to Celery from test_time_tracked_models.py.

--- common/data_refinery_common/models/test_time_tracked_models.py
+++ common/data_refinery_common/models/test_time_tracked_models.py
@@ -1,27 +1,12 @@
 from django.test import TestCase
 from django.utils import timezone
-from data_refinery_common.models import SurveyJob, Batch, File
+from data_refinery_common.models import SurveyJob
 
 
 class TimeTrackedModelTestCase(TestCase):
-    def setUp(self):
-        # Fix for issue explained here:
-        # https://stackoverflow.com/questions/31504591/interfaceerror-connection-already-closed-using-django-celery-scrapy # noqa
-        File.objects.all().delete()
-        Batch.objects.all().delete()
-        SurveyJob.objects.all().delete()
-        # Using this hackish fix because I'll be ripping celery out of
-        # the project at some point so a more thorough fix would be a
-        # waste of time
-
-        SurveyJob.objects.create(source_type="ARRAY_EXPRESS")
-
-    def tearDown(self):
-        SurveyJob.objects.all().delete()
-
     def test_time_tracking_works(self):
         """created_at and updated_at are initialized upon creation"""
-        job = SurveyJob.objects.get(source_type="ARRAY_EXPRESS")
+        job = SurveyJob.objects.create(source_type="ARRAY_EXPRESS")
         timedelta = timezone.now() - job.created_at
         self.assertLess(timedelta.total_seconds(), 1)
         self.assertEqual(job.created_at, job.updated_at)
