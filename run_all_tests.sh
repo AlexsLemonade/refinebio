d21884cfca0b63b24184ecfde015730448299fbb
Rich Jones <miserlou@gmail.com>
2018-01-24 12:34:54 -0500

    Use a volume for Dockerized PG

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -2,7 +2,6 @@
 
 # Runs all common and app specific tests.
 # This should mirror what happens in the CircleCI config (.circleci/config).
-
 ./common/run_tests.sh
 ./foreman/run_tests.sh
 ./workers/run_tests.sh
