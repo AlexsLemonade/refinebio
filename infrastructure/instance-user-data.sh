b8cd42137be6c75828494c6dfe9a64906b6c68f0
Kurt Wheeler <kurt.wheeler91@gmail.com>
2018-02-16 19:40:16 -0500

    WIP on getting Nomad installed.

--- infrastructure/instance-user-data.sh
+++ infrastructure/instance-user-data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+# set -e
 
 echo 'DOCKER_STORAGE_OPTIONS="--storage-driver overlay2"' > /etc/sysconfig/docker-storage
 /etc/init.d/docker restart
@@ -8,20 +8,25 @@ echo 'DOCKER_STORAGE_OPTIONS="--storage-driver overlay2"' > /etc/sysconfig/docke
 curl https://keybase.io/hashicorp/pgp_keys.asc | gpg --import
 
 # Download the binary and signature files.
-wget https://releases.hashicorp.com/nomad/0.7.1/nomad_0.7.1_linux_amd64.zip
-wget https://releases.hashicorp.com/nomad/0.7.1/nomad_0.7.1_SHA256SUMS
-wget https://releases.hashicorp.com/nomad/0.7.1/nomad_0.7.1_SHA256SUMS.sig
+curl -0s https://releases.hashicorp.com/nomad/0.7.1/nomad_0.7.1_linux_amd64.zip > nomad_0.7.1_linux_amd64.zip
+curl -0s https://releases.hashicorp.com/nomad/0.7.1/nomad_0.7.1_SHA256SUMS > nomad_0.7.1_SHA256SUMS
+curl -0s https://releases.hashicorp.com/nomad/0.7.1/nomad_0.7.1_SHA256SUMS.sig > nomad_0.7.1_SHA256SUMS.sig
 
 # Verify the signature file is untampered.
-if [ $(gpg --verify nomad_0.7.1_SHA256SUMS.sig nomad_0.7.1_SHA256SUMS 2>&1 grep Good) = "" ]; then
-    >&2 echo "Could not verify the signature from HashiCorp Security <security@hashicorp.com>."
+gpg_ok=$(gpg --verify nomad_0.7.1_SHA256SUMS.sig nomad_0.7.1_SHA256SUMS |& grep Good)
+if [[ "$gpg_ok" = "" ]]; then
+    echo "Could not verify the signature from HashiCorp Security <security@hashicorp.com>."
     exit 1
 fi
 
+# shasum is not on the ec2 instance. Start by getting it on there.
+
 # Verify the SHASUM matches the binary.
-if [ $(shasum -a 256 -c nomad_0.7.1_SHA256SUMS 2>&1 | grep OK) = ""]; then
-    >&2 echo "Could not verify the Nomad checksum provided by Hashicorp."
+shasum_ok=$(shasum -a 256 -c nomad_0.7.1_SHA256SUMS |& grep OK)
+if [[ "$shasum_ok" = "" ]]; then
+    echo "Could not verify the Nomad checksum provided by Hashicorp."
     exit 1
 fi
 
 unzip -d /usr/bin nomad_0.7.1_linux_amd64.zip
+chmod a+rx /usr/bin/nomad
