0842c44d64dad3614234094335ff6272e046a890
Kurt Wheeler <kurt.wheeler91@gmail.com>
2018-03-01 17:14:15 -0500

    Infrastructure code to spin up Nomad Server and Client in AWS. (#149)
    * Add infrastructure code to successfully spin up a Nomad server on an AWS instance, register jobs to it, and a Nomad Client on another instance which successfully registers itself to the cluster.

--- infrastructure/instances.tf
+++ infrastructure/instances.tf
@@ -6,7 +6,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    # This is a HVM, EBS backed SSD Ubuntu LTS AMI with Docker version 17.12 on it.
+    # This is a HVM, EBS backed SSD Ubuntu LTS AMI with Docker version 17.12.0 on it.
     values = ["ubuntu-16.04-docker-17.12.0-ce-*"]
   }
 
@@ -16,21 +16,86 @@ data "aws_ami" "ubuntu" {
   }
 }
 
-resource "aws_instance" "data_refinery_worker_1" {
+# We need to transfer the Nomad Job Specifications onto the instance
+# so we can register them. However we use these job specifications to
+# store environment variables, so they are on Github as templates. To
+# format them with the production environment variables, we need to
+# run the following command. By doing so with a `null_resource` we're
+# able to avoid having a prerequisite step to running `terraform
+# apply`.
+resource "null_resource" "format-nomad-job-specs" {
+  provisioner "local-exec" {
+    command = "cd .. && ./workers/format_nomad_with_env.sh -e prod -o $(pwd)/infrastructure/nomad-job-specs/"
+  }
+}
+
+# This script installs Nomad.
+data "local_file" "install-nomad-script" {
+  filename = "../install_nomad.sh"
+}
+
+# This is the configuration for the Nomad Server.
+data "local_file" "nomad-server-config" {
+  filename = "nomad-configuration/server.hcl"
+}
+
+# This is a Nomad Job Specification file built by ${null_resource.format-nomad-job-specs}.
+data "local_file" "downloader-job-spec" {
+  filename = "nomad-job-specs/downloader.nomad"
+}
+
+# This is another Nomad Job Specification file built by ${null_resource.format-nomad-job-specs}.
+data "local_file" "processor-job-spec" {
+  filename = "nomad-job-specs/processor.nomad"
+}
+
+# This script smusher exists in order to be able to circumvent a
+# limitation of AWS which is that you get one script and one script
+# only to set up the instance when it boots up. Because there is only
+# one script you cannot place additional files your script may need
+# onto the instance. Therefore this script smusher templates the files
+# the instance-user-data.sh script needs into it, so that once it
+# makes its way onto the instance it can spit them back out onto the
+# disk.
+data "template_file" "nomad-server-script-smusher" {
+  template = "${file("nomad-configuration/nomad-server-instance-user-data.tpl.sh")}"
+
+  vars {
+    downloader_job_spec = "${data.local_file.downloader-job-spec.content}"
+    processor_job_spec = "${data.local_file.processor-job-spec.content}"
+    install_nomad_script = "${data.local_file.install-nomad-script.content}"
+    nomad_server_config = "${data.local_file.nomad-server-config.content}"
+  }
+}
+
+# This instance will run the Nomad Server. We will want three of
+# these, but one server needs to start first so that the others can be
+# aware of its IP address to join it.
+# Those other servers do not yet exist in the current configuration.
+# TODO: create those other servers!
+resource "aws_instance" "nomad-server-1" {
   ami = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.xlarge"
+  instance_type = "t2.small"
   availability_zone = "us-east-1a"
   vpc_security_group_ids = ["${aws_security_group.data_refinery_worker.id}"]
   iam_instance_profile = "${aws_iam_instance_profile.ecs_instance_profile.name}"
   subnet_id = "${aws_subnet.data_refinery_1a.id}"
   depends_on = ["aws_internet_gateway.data_refinery"]
-  user_data = "${file("server-instance-user-data.sh")}"
   key_name = "${aws_key_pair.data_refinery.key_name}"
 
+  # Our instance-user-data.sh script is built by Terraform at
+  # apply-time so that it can put additional files onto the
+  # instance. For more information see the definition of this resource.
+  user_data = "${data.template_file.nomad-server-script-smusher.rendered}"
+
   tags = {
-    Name = "data-refinery-1-${var.user}-${var.stage}"
+    Name = "nomad-server-1-${var.user}-${var.stage}"
   }
 
+  # Nomad server requirements can be found here:
+  # https://www.nomadproject.io/guides/cluster/requirements.html
+  # However I do not think that these accurately reflect those requirements.
+  # I think these are the defaults provided in terraform examples.
   root_block_device = {
     volume_type = "gp2"
     volume_size = 100
@@ -43,21 +108,52 @@ resource "aws_instance" "data_refinery_worker_1" {
   }
 }
 
-resource "aws_instance" "data_refinery_worker_2" {
+output "nomad-server-ip" {
+  value = "aws_instance.nomad-server-1.public_ip"
+}
+
+# The Nomad Client needs to be aware of the Nomad Server's IP address,
+# so we template it into its configuration.
+data "template_file" "nomad-client-config" {
+  template = "${file("nomad-configuration/client.tpl.hcl")}"
+
+  vars {
+    nomad_server_address = "${aws_instance.nomad-server-1.private_ip}"
+  }
+}
+
+# This script smusher serves a similar purpose to
+# ${data.template_file.nomad-server-script-smusher} but for the Nomad
+# Client. See that resource's definition for more information.
+data "template_file" "nomad-client-script-smusher" {
+  template = "${file("nomad-configuration/nomad-client-instance-user-data.tpl.sh")}"
+
+  vars {
+    install_nomad_script = "${data.local_file.install-nomad-script.content}"
+    nomad_client_config = "${data.template_file.nomad-client-config.rendered}"
+  }
+}
+
+# This instance will run the Nomad Client. We will eventually want
+# these to be spot instances, but it is much simpler operationally to
+# do it using a normal ec2 instance.
+resource "aws_instance" "nomad-client-1" {
   ami = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.xlarge"
   availability_zone = "us-east-1b"
   vpc_security_group_ids = ["${aws_security_group.data_refinery_worker.id}"]
   iam_instance_profile = "${aws_iam_instance_profile.ecs_instance_profile.name}"
   subnet_id = "${aws_subnet.data_refinery_1b.id}"
-  depends_on = ["aws_internet_gateway.data_refinery", "aws_instance.data_refinery_worker_1"]
-  user_data = "${file("client-instance-user-data.sh")}"
+  depends_on = ["aws_internet_gateway.data_refinery", "aws_instance.nomad-server-1"]
+  user_data = "${data.template_file.nomad-client-script-smusher.rendered}"
   key_name = "${aws_key_pair.data_refinery.key_name}"
 
   tags = {
-    Name = "data-refinery-2-${var.user}-${var.stage}"
+    Name = "nomad-client-1-${var.user}-${var.stage}"
   }
 
+  # I think these are the defaults provided in terraform examples.
+  # They should be removed or revisited.
   root_block_device = {
     volume_type = "gp2"
     volume_size = 100
@@ -68,6 +164,12 @@ resource "aws_instance" "data_refinery_worker_2" {
     volume_type = "gp2"
     volume_size = 40
   }
+}
+
+
+
+output "nomad-client-ip" {
+  value = "aws_instance.nomad-client-1.public_ip"
 }
 
 variable "database_password" {}
---
+++ infrastructure/nomad-configuration/nomad-client-instance-user-data.tpl.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# This is a template for the instance-user-data.sh script for the nomad client.
+# For more information on instance-user-data.sh scripts, see:
+# https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/user-data.html
+
+# This script will be formatted by Nomad, which will read files from
+# the project into terraform variables, and then template them into
+# the following Here Documents. These will then be written out to files
+# so that they can be used. A more ideal solution than this would be if
+# we could just give AWS a list of files to put onto the instance for us,
+# but they only give us this one script to do it with. Nomad has file
+# provisioners which will put files onto the instance after it starts up,
+# but those run after this script runs.
+
+
+# Change to home directory of the default user
+cd /home/ubuntu
+
+
+# Output the files we need to start up Nomad and register jobs:
+# (Note that the lines starting with "$" are where
+#  Terraform will template in the contents of those files.)
+
+# Create the script to install Nomad.
+cat <<"EOF" > install_nomad.sh
+${install_nomad_script}
+EOF
+
+# Create the Nomad Client configuration.
+cat <<"EOF" > client.hcl
+${nomad_client_config}
+EOF
+
+
+# Create a directory for docker to use as a volume.
+mkdir /home/ubuntu/docker_volume
+chmod a+rwx /home/ubuntu/docker_volume
+
+
+# Install Nomad
+chmod +x install_nomad.sh
+./install_nomad.sh
+
+# Start the Nomad agent in client mode.
+nomad agent -config client.hcl > /tmp/nomad_client.log &
