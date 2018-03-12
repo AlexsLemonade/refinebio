4755021e7b55fd1e6fbe900e4da7dbe72d224387
Dongbo <dongbo.hu@gmail.com>
2018-03-12 15:53:02 -0400

    Tweak scripts

--- .circleci/run_terraform.sh
+++ .circleci/run_terraform.sh
@@ -1,24 +1,23 @@
-# TODO: remove ".s3" suffix"
-
+# Install terraform
 cd
 wget https://releases.hashicorp.com/terraform/0.11.3/terraform_0.11.3_linux_amd64.zip
 unzip terraform_0.11.3_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
 
-cd ~/refineio/infrastructure
+cd ~/refinebio/infrastructure
 
 # Download encrypted tfstate files from S3
 aws s3 cp s3://refinebio-tfstate/$TFSTATE.enc .
 aws s3 cp s3://refinebio-tfstate/$TFSTATE_BAK.enc .
 
 # Decrypt tfstate files
-openssl aes-256-cbc -d -in $TFSTATE.enc -out $TFSTATE.s3 -k $OPENSSL_KEY
-openssl aes-256-cbc -d -in $TFSTATE_BAK.enc -out $TFSTATE_BAK.s3 -k $OPENSSL_KEY
+openssl aes-256-cbc -d -in $TFSTATE.enc -out $TFSTATE -k $OPENSSL_KEY
+openssl aes-256-cbc -d -in $TFSTATE_BAK.enc -out $TFSTATE_BAK -k $OPENSSL_KEY
 
 # New deployment
-#terraform init
-#terraform plan
-#terraform apply -auto-approve
+terraform init
+terraform plan
+terraform apply -auto-approve
 
 # Encrypt new tfstate files
 openssl aes-256-cbc -e -in $TFSTATE -out $TFSTATE.enc -k $OPENSSL_KEY
--- .circleci/update_docker_img.sh
+++ .circleci/update_docker_img.sh
@@ -1,16 +1,11 @@
-CIRCLE_TAG=dhutest # dhu test only
-
 cd ~/refinebio
 # Build docker images
-docker build -t ccdl/test_worker_base:$CIRCLE_TAG -f workers/Dockerfile.base .
-docker build -t ccdl/test_workers:$CIRCLE_TAG -f workers/Dockerfile .
-docker build -t ccdl/test_foreman:$CIRCLE_TAG -f foreman/Dockerfile .
+docker build -t ccdl/data_refinery_worker_base:$CIRCLE_TAG -f workers/Dockerfile.base .
+docker build -t ccdl/data_refinery_workers:$CIRCLE_TAG -f workers/Dockerfile .
+docker build -t ccdl/data_refinery_foreman:$CIRCLE_TAG -f foreman/Dockerfile .
 
-
-# Log in and push images
+# Log into DockerHub and push images
 docker login -u $DOCKER_ID -p $DOCKER_PASSWD
-
-# Push images
-docker push ccdl/test_worker_base:$CIRCLE_TAG
-docker push ccdl/test_worker:$CIRCLE_TAG
-docker push ccdl/test_foreman:$CIRCLE_TAG
+docker push ccdl/data_refinery_worker_base:$CIRCLE_TAG
+docker push ccdl/data_refinery_workers:$CIRCLE_TAG
+docker push ccdl/data_refinery_foreman:$CIRCLE_TAG
