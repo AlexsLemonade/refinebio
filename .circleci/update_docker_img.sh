12fffe56093e835ea8b55ffe90d90282f422e450
Dongbo <dongbo.hu@gmail.com>
2018-03-13 12:10:57 -0400

    Push an image right after build

--- .circleci/update_docker_img.sh
+++ .circleci/update_docker_img.sh
@@ -1,11 +1,14 @@
 cd ~/refinebio
-# Build docker images
-docker build -t ccdl/data_refinery_worker_base:$CIRCLE_TAG -f workers/Dockerfile.base .
-docker build -t ccdl/data_refinery_workers:$CIRCLE_TAG -f workers/Dockerfile .
-docker build -t ccdl/data_refinery_foreman:$CIRCLE_TAG -f foreman/Dockerfile .
 
-# Log into DockerHub and push images
+# Log into DockerHub
 docker login -u $DOCKER_ID -p $DOCKER_PASSWD
+
+# Build docker images and push them into DockerHub
+docker build -t ccdl/data_refinery_worker_base:$CIRCLE_TAG -f workers/Dockerfile.base .
 docker push ccdl/data_refinery_worker_base:$CIRCLE_TAG
+
+docker build -t ccdl/data_refinery_workers:$CIRCLE_TAG -f workers/Dockerfile .
 docker push ccdl/data_refinery_workers:$CIRCLE_TAG
+
+docker build -t ccdl/data_refinery_foreman:$CIRCLE_TAG -f foreman/Dockerfile .
 docker push ccdl/data_refinery_foreman:$CIRCLE_TAG
