23653ae440c6a91f2e6479748637fb6d42088527
Dongbo <dongbo.hu@gmail.com>
2018-03-14 12:18:13 -0400

    Add more comments

--- .circleci/update_docker_img.sh
+++ .circleci/update_docker_img.sh
@@ -2,23 +2,25 @@
 # required by data_refinery_workers and data_refinery_foreman images.
 cd ~/refinebio/common && python setup.py sdist
 
-cd ~/refinebio
 # Log into DockerHub
+cd ~/refinebio
 docker login -u $DOCKER_ID -p $DOCKER_PASSWD
 
-# Build docker images and push them into DockerHub
+# Build and push worker_base image
 docker build -t ccdl/data_refinery_worker_base:$CIRCLE_TAG -f workers/Dockerfile.base .
 docker push ccdl/data_refinery_worker_base:$CIRCLE_TAG
 # Update latest version
 docker tag ccdl/data_refinery_worker_base:$CIRCLE_TAG ccdl/data_refinery_worker_base:latest
 docker push ccdl/data_refinery_worker_base:latest
 
+# Build and push workers image
 docker build -t ccdl/data_refinery_workers:$CIRCLE_TAG -f workers/Dockerfile .
 docker push ccdl/data_refinery_workers:$CIRCLE_TAG
 # Update latest version
 docker tag ccdl/data_refinery_workers:$CIRCLE_TAG ccdl/data_refinery_workers:latest
 docker push ccdl/data_refinery_workers:latest
 
+# Build and push foreman image
 docker build -t ccdl/data_refinery_foreman:$CIRCLE_TAG -f foreman/Dockerfile .
 docker push ccdl/data_refinery_foreman:$CIRCLE_TAG
 # Update latest version
