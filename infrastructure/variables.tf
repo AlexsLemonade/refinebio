01d68a35568367c26c1d8e136a45fb3d0102c47b
Rich Jones <miserlou@gmail.com>
2018-03-14 12:18:05 -0400

    rjones -> myusername

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -6,7 +6,7 @@
 # as default values for variables. So `TF_VAR_user=rjones tf plan`, etc.
 # Also, don't use any non-alphanumeric characters here or RDS will whinge.
 variable "user" {
-  default = "otherkurt"
+  default = "myusername"
 }
 
 variable "stage" {
