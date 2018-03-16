4007530eb779a45e08172664b1fbb5515bc20408
Dongbo <dongbo.hu@gmail.com>
2018-03-16 16:34:40 -0400

    Install nomad before deployment

--- .circleci/run_terraform.sh
+++ .circleci/run_terraform.sh
@@ -1,15 +1,18 @@
-# Install terraform
+# Install terraform and nomad
 cd
 wget https://releases.hashicorp.com/terraform/0.11.3/terraform_0.11.3_linux_amd64.zip
 unzip terraform_0.11.3_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
 
+apt-get update
+apt-get install lxc -y
+wget https://releases.hashicorp.com/nomad/0.7.1/nomad_0.7.1_linux_amd64-lxc.zip
+unzip nomad_0.7.1_linux_amd64-lxc.zip
+sudo mv nomad /usr/local/bin/
+
 cd ~/refinebio/infrastructure
 terraform init
-
-TF_VAR_user=deploy
-TV_VAR_stage=production
-BUCKET_NAME=refinebio-tfstate-${TF_VAR_user}-${TF_VAR_stage}
+BUCKET_NAME=refinebio-tfstate-deploy-production
 
 # Download encrypted tfstate files from S3
 aws s3 cp s3://$BUCKET_NAME/$TFSTATE.enc .
@@ -20,7 +23,7 @@ openssl aes-256-cbc -d -in $TFSTATE.enc -out $TFSTATE -k $OPENSSL_KEY
 openssl aes-256-cbc -d -in $TFSTATE_BAK.enc -out $TFSTATE_BAK -k $OPENSSL_KEY
 
 # New deployment
-./deploy.sh
+TF_VAR_user=deploy TV_VAR_stage=production ./deploy.sh
 
 # Encrypt new tfstate files
 openssl aes-256-cbc -e -in $TFSTATE -out $TFSTATE.enc -k $OPENSSL_KEY
