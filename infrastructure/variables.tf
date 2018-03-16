cdc91c348b07273f4261674aa1ff278834de6e93
Rich Jones <rich@anomos.info>
2018-03-16 11:17:44 -0400

    Add Log Consumption To Readme // Add EFS Mounting (#152)
    * document awslogs
    
    * a little reorg
    
    * Add EFS disk, mount points and mounting script
    
    * explicit sudo
    
    * set volume dir to /var/efs on prod
    
    * Update/upgrade before nfs install
    
    * add surveyor jobs
    
    * incramental progress towards it actually working
    
    * install rds upon client init
    
    * devops is killing me
    
    * the slow and steady march of progress continues. now connects to the correct database, just needs a db migrate step somewhere
    
    * Adds deployment features for infrastructure/other stuff.
    
    * just sharing what i have
    
    * losing my hair over this
    
    * actually running crunchies in the cloud now
    
    * Makes deploy.sh do what we've been wanting it to. There's still some ugliness including environment var stuff but this is so much easier to work with already.
    
    * Readd awslogs to surveyor'
    
    * Store/output all prod env vars in variables.tf, use them via deploy.sh, and harden deploy.sh a bit.
    
    * Fix overwriting default username.
    
    * It is not the job of the deploy script to manage system dependencies. Also those dont work on OSX anyway
    
    * rjones -> myusername
    
    * ensure nomad hosts, correct log stream
    
    * use internal nomad ip address, nuke worker prod file
    
    * Change to using new images and streamlines stopping Nomad jobs.
    
    * Stop making migrations at deploy time, we'll need them baked into the image.
    
    * Removes namespaced database on the namespaced database-instance so we're only using data_refinery. Also removes remaining ECS references.
    
    * Fixes EFS and cleans up Nomad jobs when stopping them.
    
    * Make postgres engine version match Docker version match RDS latest version
    
    * Make one script to format all nomad jobs rather than having two nearly identical scripts.
    
    * Fixes AWS env var collision and enables removing circle ingress at the end of deploy.sh.
    
    * get external ip address from DNS rather than random web service
    
    * purge watchtower
    
    * small typo
    
    * comment on broken rds final snapshot
    
    * begin iam clampdown
    
    * mention deploy.sh in readme
    
    * a few comments in the spec formatter
    
    * more comments, conditionally delete cloudwatch logs
    
    * Remove some database provisioning cruft, add another availabilty zone
    
    * slightly better comment
    
    * Kurt is a stickler about these parens
    
    * Add narrative documentation to deploy.sh script

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -4,8 +4,9 @@
 
 # Annoyingly, TF can't have computed variables (${env.USER})
 # as default values for variables. So `TF_VAR_user=rjones tf plan`, etc.
+# Also, don't use any non-alphanumeric characters here or RDS will whinge.
 variable "user" {
-  default = "my-user-name"
+  default = "myusername"
 }
 
 variable "stage" {
@@ -14,4 +15,118 @@ variable "stage" {
 
 variable "region" {
   default = "us-east-1"
+}
+
+variable "host_ip" {
+  # This will be overwritten.
+  default = "127.0.0.1"
+}
+
+variable "database_user" {
+  default = "drpostgresuser"
+}
+
+variable "database_password" {
+  # This will be overwritten by the password in terraform.tfvars.
+  # It's kept there so it's secret.
+  default = "drpostgrespassword"
+}
+
+variable "django_secret_key" {
+  # TODO: generate a new one of these and store it in terraform.tfvars as well.
+  default = "NtG1bxZU115GThwrLuAJe0PhTVN9hJ4P"
+}
+variable "django_debug" {
+  default = "False"
+}
+variable "database_port" {
+  default = "5432"
+}
+
+variable "database_timeout" {
+  default = "30"
+}
+
+variable "running_in_cloud" {
+  default = "True"
+}
+
+# This is a placeholder until there is a production image ready.
+variable "workers_docker_image" {
+  default = "wkurt/dr_worker:3"
+}
+variable "foreman_docker_image" {
+  default = "wkurt/dr_foreman:4"
+}
+variable "use_s3" {
+  default = "True"
+}
+variable "local_root_dir" {
+  default = "/home/user/data_store"
+}
+variable "raw_prefix" {
+  default = "raw"
+}
+variable "temp_prefix" {
+  default = "temp"
+}
+variable "processed_prefix" {
+  default = "processed"
+}
+
+# Output our production environment variables. These should be in
+# parity with the env files such as workers/environments/dev.
+output "environment_variables" {
+  value = [
+    {name = "REGION"
+      value = "${var.region}"},
+    {name = "USER"
+      value = "${var.user}"},
+    {name = "STAGE"
+      value = "${var.stage}"},
+    {name = "AWS_ACCESS_KEY_ID_WORKER"
+      value = "${aws_iam_access_key.data_refinery_user_worker_key.id}"},
+    {name = "AWS_SECRET_ACCESS_KEY_WORKER"
+      value = "${aws_iam_access_key.data_refinery_user_worker_key.secret}"},
+    {name = "DJANGO_DEBUG"
+      value = "${var.django_debug}"},
+    {name = "DJANGO_SECRET_KEY"
+      value = "${var.django_secret_key}"},
+    {name = "DATABASE_NAME"
+      value = "${aws_db_instance.postgres_db.name}"},
+    {name = "DATABASE_HOST"
+      value = "${aws_db_instance.postgres_db.address}"},
+    {name = "DATABASE_USER"
+      value = "${var.database_user}"},
+    {name = "DATABASE_PASSWORD"
+      value = "${var.database_password}"},
+    {name = "DATABASE_PORT"
+      value = "${var.database_port}"},
+    {name = "DATABASE_TIMEOUT"
+      value = "${var.database_timeout}"},
+    {name = "RUNNING_IN_CLOUD"
+      value = "${var.running_in_cloud}"},
+    {name = "USE_S3"
+      value = "${var.use_s3}"},
+    {name = "S3_BUCKET_NAME"
+      value = "${aws_s3_bucket.data_refinery_bucket.id}"},
+    {name = "LOCAL_ROOT_DIR"
+      value = "${var.local_root_dir}"},
+    {name = "RAW_PREFIX"
+      value = "${var.raw_prefix}"},
+    {name = "TEMP_PREFIX"
+      value = "${var.temp_prefix}"},
+    {name = "PROCESSED_PREFIX"
+      value = "${var.processed_prefix}"},
+    {name = "WORKERS_DOCKER_IMAGE"
+      value = "${var.workers_docker_image}"},
+    {name = "FOREMAN_DOCKER_IMAGE"
+      value = "${var.foreman_docker_image}"},
+    {name = "NOMAD_HOST"
+      value = "${aws_instance.nomad_server_1.private_ip}"},
+    {name = "NOMAD_PUBLIC_HOST"
+      value = "${aws_instance.nomad_server_1.public_ip}"},
+    {name = "NOMAD_PORT"
+      value = "4646"}
+  ]
 }
