b97408fdef0180af0e136d5e5ec7dcaa26fed382
Rich Jones <miserlou@gmail.com>
2018-03-21 10:33:50 -0400

    80% coverage of common

--- common/data_refinery_common/models/test_jobs.py
+++ common/data_refinery_common/models/test_jobs.py
@@ -4,3 +4,19 @@ from data_refinery_common.models import (
     DownloaderJob,
     ProcessorJob,
 )
+
+class SanityTestJobsTestCase(TestCase):
+    def test_jobs_sanity(self):
+        """Just makes sure creating Jobs doesn't fail"""
+
+        s_job = SurveyJob()
+        s_job.save()
+
+        processor_job = ProcessorJob()
+        processor_job.pipeline_applied = "test0"
+        processor_job.save()
+
+        dl_job = DownloaderJob()
+        dl_job.downloader_task = "XYZ"
+        dl_job.accession_code = "123"
+        dl_job.save()
