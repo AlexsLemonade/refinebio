9bb7145ddad7078bb67f092902b8aac8bb7fa10c
Rich Jones <miserlou@gmail.com>
2018-03-23 11:51:39 -0400

    paginate experiments list

--- api/data_refinery_api/settings.py
+++ api/data_refinery_api/settings.py
@@ -143,3 +143,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+##
+# Django Rest Framework
+##
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
+}
--- api/data_refinery_api/views.py
+++ api/data_refinery_api/views.py
@@ -1,4 +1,7 @@
+from django.conf import settings
+from rest_framework.settings import api_settings
 from django.http import Http404
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -6,14 +9,52 @@ from rest_framework import status
 from data_refinery_common.models import Experiment
 from data_refinery_api.serializers import ExperimentSerializer
 
-class ExperimentList(APIView):
+class PaginatedAPIView(APIView):
+    pagination_class = api_settings.DEFAULT_PAGINATION_CLASS
+
+    @property
+    def paginator(self):
+        """
+        The paginator instance associated with the view, or `None`.
+        """
+        if not hasattr(self, '_paginator'):
+            if self.pagination_class is None:
+                self._paginator = None
+            else:
+                self._paginator = self.pagination_class()
+        return self._paginator
+
+    def paginate_queryset(self, queryset):
+        """
+        Return a single page of results, or `None` if pagination is disabled.
+        """
+        if self.paginator is None:
+            return None
+        return self.paginator.paginate_queryset(queryset, self.request, view=self)
+
+    def get_paginated_response(self, data):
+        """
+        Return a paginated style `Response` object for the given output data.
+        """
+        assert self.paginator is not None
+        return self.paginator.get_paginated_response(data)
+
+
+class ExperimentList(PaginatedAPIView):
     """
     List all Experiments
     """
+
     def get(self, request, format=None):
         experiments = Experiment.objects.all()
-        serializer = ExperimentSerializer(experiments, many=True)
-        return Response(serializer.data)
+
+        page = self.paginate_queryset(experiments)
+        if page is not None:
+            serializer = ExperimentSerializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        else:
+	        serializer = ExperimentSerializer(experiments, many=True)
+	        return Response(serializer.data)
 
 class ExperimentDetail(APIView):
     """
