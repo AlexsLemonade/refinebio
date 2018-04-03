cd0ccedd4913aafae0b3485d9133b6257cbf97a4
Rich Jones <miserlou@gmail.com>
2018-04-03 13:22:51 -0400

    timeout requests

--- foreman/data_refinery_foreman/surveyor/array_express.py
+++ foreman/data_refinery_foreman/surveyor/array_express.py
@@ -60,7 +60,7 @@ class ArrayExpressSurveyor(ExternalSourceSurveyor):
         See an example at: https://www.ebi.ac.uk/arrayexpress/json/v3/experiments/E-MTAB-3050/sample
         """
         request_url = EXPERIMENTS_URL + experiment_accession_code;
-        experiment_request = requests.get(request_url)
+        experiment_request = requests.get(request_url, timeout=15)
         try:
             parsed_json = experiment_request.json()["experiments"]["experiment"][0]
         except KeyError:
@@ -144,7 +144,7 @@ class ArrayExpressSurveyor(ExternalSourceSurveyor):
             ## Fetch and parse the IDF file for any other fields
             IDF_URL_TEMPLATE = "https://www.ebi.ac.uk/arrayexpress/files/{code}/{code}.idf.txt"
             idf_url = IDF_URL_TEMPLATE.format(code=experiment_accession_code)
-            idf_text = requests.get(idf_url).text
+            idf_text = requests.get(idf_url, timeout=15).text
             lines = idf_text.split('\n')
             idf_dict = {}
             for line in lines:
@@ -222,7 +222,7 @@ class ArrayExpressSurveyor(ExternalSourceSurveyor):
 
         created_samples = []
 
-        r = requests.get(SAMPLES_URL.format(experiment.accession_code))
+        r = requests.get(SAMPLES_URL.format(experiment.accession_code), timeout=15)
         samples = r.json()["experiment"]["sample"]
 
         try:
