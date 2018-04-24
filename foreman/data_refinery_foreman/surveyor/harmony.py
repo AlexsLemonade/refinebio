33826cabf84fbc70f8fc2f4e666c070393261d82
Rich Jones <miserlou@gmail.com>
2018-04-24 10:52:51 -0400

    add crude geo harmonizer

--- foreman/data_refinery_foreman/surveyor/harmony.py
+++ foreman/data_refinery_foreman/surveyor/harmony.py
@@ -166,6 +166,97 @@ def harmonize(metadata):
          'submission_comment': 'submission brokered by GEO',
          'submission_title': 'Submitted by Gene Expression Omnibus on 25-SEP-2017'}
 
+    GEO:
+        ex:
+            {'channel_count': ['1'],
+             'characteristics_ch1': ['patient: P-39',
+                                     'gender: female',
+                                     'age: 65',
+                                     'location: lower leg',
+                                     'transplanted organ: kidney',
+                                     'immunosuppressive drugs: azathioprine + prednison',
+                                     'sample type: squamous cell carcinoma',
+                                     'cell type: keratinocyte'],
+             'contact_address': ['Einthovenweg 20'],
+             'contact_city': ['Leiden'],
+             'contact_country': ['Netherlands'],
+             'contact_department': ['Toxicogenetics, S4-P'],
+             'contact_email': ['h.vrieling@lumc.nl'],
+             'contact_institute': ['Leiden University Medical Center'],
+             'contact_laboratory': ['room T4-34'],
+             'contact_name': ['Harry,,Vrieling'],
+             'contact_zip/postal_code': ['2333 ZC'],
+             'data_processing': ['Raw data was extracted from the BeadChip data files in '
+                                 'Illumina’s BeadStudio Version 3.2 software using the '
+                                 'gene expression module (v 3.2.7). Background subtracted '
+                                 'data was further analyzed in R-based Bioconductor '
+                                 'package, lumi (version 1.12.4). In lumi, the data was '
+                                 'transformed (variance-stabilizing transformation (VST)) '
+                                 'and normalized (robust spline normalization (RSN)), '
+                                 'resulting in log-transformed normalized data. The '
+                                 'R-package illuminaHumanv2.db (version 1.4.1) was used '
+                                 'for annotation. The data were purged of genes that did '
+                                 'not meet the detection limit (expression-detection '
+                                 'P-value >0.01) and/or were not annotated. The limma '
+                                 'R-package (version 3.2.3) was used to identify '
+                                 'differentially expressed genes (DEGs) between SCC, AK '
+                                 'and NS. Gene set enrichment analysis (GSEA) was '
+                                 'performed with the significantly DEGs from the limma '
+                                 'analysis using DAVID Bioinformatic Resources v6.7 '
+                                 '(http://david.abcc.ncifcrf.gov). GSEA on the entire data '
+                                 'set was performed using the parametric gene set '
+                                 'enrichment analysis (PGSEA) R-package (version '
+                                 '1.14.0).      To identify activation of transcription '
+                                 'factors in AKs and SCCs, the DEGs from the limma '
+                                 'analysis were investigated using the online analysis '
+                                 'tool oPOSSUM.',
+                                 'Matrix normalized matrix shows VST-transformed, '
+                                 'RSN-normalized data (used scripts from lumi package)',
+                                 'Matrix non-normalized: AVG_Signal: average signal for '
+                                 'the probe; BEAD_STDERR: standard error of the beads; '
+                                 'Avg_NBEADS: average number of beads for that probe; '
+                                 'Detection Pval: detection p-value. All extracted from '
+                                 'Beadstudio'],
+             'data_row_count': ['48701'],
+             'description': ['1881436235_A'],
+             'extract_protocol_ch1': ['RNA was isolated from SCC and AK samples that '
+                                      'contained at least 70% tumor cells, as determined '
+                                      'by haematoxylin and eosin stained frozen sections. '
+                                      'From the sample of unexposed NS the epidermis was '
+                                      'removed for further processing by cryosectioning '
+                                      'parallel to the outer surface of the skin biopsy. '
+                                      'RNA was extracted from frozen material using the '
+                                      'RNeasy Fibrous Tissue kit (Qiagen), which included '
+                                      'proteinase K treatment (10 min at 55˚C) of the '
+                                      'lysed sample in RLT-buffer and on-column DNase '
+                                      'treatment. RNA was quantified using a Nanodrop '
+                                      '(NanoDrop technologies) and evaluated for '
+                                      'degradation with a RNA 6000 Nano Labchip on the '
+                                      '2100 Bioanalyzer (Agilent Technologies)'],
+             'geo_accession': ['GSM808778'],
+             'hyb_protocol': ['The standard Illumina hybridization protocol was used. In '
+                              'brief, the samples were hybridized to the arrays at 58ºC '
+                              'overnight.'],
+             'label_ch1': ['biotin'],
+             'label_protocol_ch1': ['100 ng of total RNA was converted to cDNA and '
+                                    'subsequently labeled cRNA using the Ambion Illumina '
+                                    'TotalPrep RNA Amplification kit (Ambion) according to '
+                                    'manufacturer’s instructions'],
+             'last_update_date': ['Feb 06 2013'],
+             'molecule_ch1': ['total RNA'],
+             'organism_ch1': ['Homo sapiens'],
+             'platform_id': ['GPL6102'],
+             'scan_protocol': ['The beadChips were scanned using the Illumina BeadArray '
+                               'Reader, using the standard Illumina scanning protocol'],
+             'series_id': ['GSE32628', 'GSE32969', 'GSE32979'],
+             'source_name_ch1': ['cutaneous squamous cell carcinoma'],
+             'status': ['Public on Feb 06 2013'],
+             'submission_date': ['Oct 05 2011'],
+             'supplementary_file': ['NONE'],
+             'taxid_ch1': ['9606'],
+             'title': ['SCC_P-39'],
+             'type': ['RNA']}
+
     """
 
     # Prepare the harmonized samples
@@ -224,7 +315,8 @@ def harmonize(metadata):
                 break
 
     ##
-    # Cell Parts!
+    # Organ Parts!
+    # XXX: Cell Type and Organ Type are different
     ##
     part_fields = [
                     # AE
@@ -251,6 +343,9 @@ def harmonize(metadata):
 
                     # SRA
                     'sample_cell_type'
+
+                    # GEO
+                    'sample type'
                 ]
     part_fields = add_variants(part_fields)
     for sample in original_samples:
@@ -322,7 +417,7 @@ def harmonize(metadata):
     # Cell Line!
     ##
     cell_line_fields = [
-                    'cell_line'
+                    'cell line'
                 ]
     cell_line_fields = add_variants(cell_line_fields)
     for sample in original_samples:
--- foreman/data_refinery_foreman/surveyor/test_harmony.py
+++ foreman/data_refinery_foreman/surveyor/test_harmony.py
@@ -1,5 +1,7 @@
-import json
 import datetime
+import GEOparse
+import json
+
 from unittest.mock import Mock, patch, call
 from django.test import TestCase
 from data_refinery_common.job_lookup import Downloaders
@@ -12,6 +14,7 @@ from data_refinery_common.models import (
 )
 from data_refinery_foreman.surveyor.array_express import ArrayExpressSurveyor
 from data_refinery_foreman.surveyor.sra import SraSurveyor
+from data_refinery_foreman.surveyor.geo import GeoSurveyor
 from data_refinery_foreman.surveyor.harmony import harmonize, parse_sdrf
 
 class HarmonyTestCase(TestCase):
@@ -29,6 +32,7 @@ class HarmonyTestCase(TestCase):
         metadata = parse_sdrf("https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-3050/E-MTAB-3050.sdrf.txt")
         for sample in metadata:
             sample['title'] = sample['Assay Name']
+
         harmonized = harmonize(metadata)
         print(harmonized)
 
@@ -40,4 +44,27 @@ class HarmonyTestCase(TestCase):
         metadata = SraSurveyor.gather_all_metadata("SRR6718414")
         metadata['title'] = metadata['sample_title']
         harmonized = harmonize([metadata])
-        print(harmonized)
+        #print(harmonized)
+
+    def test_geo_harmony(self):
+        """
+
+        """
+        
+        gse = GEOparse.get_GEO("GSE32628", destdir='/tmp')
+        
+        preprocessed_samples = []
+        for sample_id, sample in gse.gsms.items():
+            new_sample = {}
+            for key, value in sample.metadata.items():
+
+                if key == "characteristics_ch1":
+                    for pair in value:
+                        split = pair.split(':')
+                        new_sample[split[0].strip()] = split[1].strip()
+                    continue
+
+                new_sample[key] = value[0]
+            preprocessed_samples.append(new_sample)
+            harmonized = harmonize(preprocessed_samples)
+            #print(harmonized)
