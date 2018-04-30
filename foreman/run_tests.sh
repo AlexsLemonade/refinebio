7bb7c429b3fc5f5bff59a30b680a53212c9616b4
Kurt Wheeler <kurt.wheeler91@gmail.com>
2018-04-30 14:35:46 -0400

    Unmangles volume_directory in foreman/run_tests.sh.

--- foreman/run_tests.sh
+++ foreman/run_tests.sh
@@ -10,7 +10,7 @@ script_directory=`perl -e 'use File::Basename;
 cd $script_directory
 
 # Set up the data volume directory if it does not already exist
-volume_directory="$script_directory/workers/test_volume"
+volume_directory="$script_directory/test_volume"
 if [ ! -d "$volume_directory" ]; then
     mkdir $volume_directory
     chmod -R a+rwX $volume_directory
