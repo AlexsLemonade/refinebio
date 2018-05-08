88ee073651eb9f40973b074c4f7227e7cc4b90ed
Rich Jones <miserlou@gmail.com>
2018-05-08 14:01:47 -0400

    add email to create

--- api/data_refinery_api/serializers.py
+++ api/data_refinery_api/serializers.py
@@ -326,6 +326,7 @@ class CreateDatasetSerializer(serializers.ModelSerializer):
         fields = (
                     'id',
                     'data',
+                    'email_address'
             )
 
     def validate(self, data):
