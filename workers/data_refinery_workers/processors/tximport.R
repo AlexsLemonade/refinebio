4cb897ded935e6892a602ab5b1bf3f08f94bc3d4
Dongbo <dongbo.hu@gmail.com>
2018-05-24 11:04:58 -0400

    Changes based on Jackie's comments

--- workers/data_refinery_workers/processors/tximport.R
+++ workers/data_refinery_workers/processors/tximport.R
@@ -10,54 +10,52 @@
 # (2) TPM file (saved as <experiment_dir>/gene_lengthScaledTPM.tsv)
 
 # Handle input arguments:
-library("optparse")
 option_list <- list(
-  make_option("--exp_dir", type = "character"),
-  make_option("--gene2txmap", type = "character")
+  optparse::make_option("--exp_dir", type = "character"),
+  optparse::make_option("--gene2txmap", type = "character")
 )
 
-opt_parser <- OptionParser(option_list = option_list)
-opt <- parse_args(opt_parser)
+opt_parser <- optparse::OptionParser(option_list = option_list)
+opt <- optparse::parse_args(opt_parser)
 
 # directory of the experiment data
-exp.directory <- opt$exp_dir
+exp_dir <- opt$exp_dir
 
 # Each sample's data is in a sub-dir of experiment directory.
-samples <- list.dirs(exp.directory, full.names = FALSE, recursive = FALSE)
+samples <- list.dirs(exp_dir, full.names = FALSE, recursive = FALSE)
 
 # Get a list of "quant.sf" files in experiment directory.
 # The paths of "quant.sf" files are in this format:
 # <exp_dir>/<sample>/processed/quant.sf
-sf.files <- list.files(exp.directory, pattern="^quant.sf$", full.names = TRUE, recursive = TRUE)
+sf_files <- list.files(exp_dir, pattern="^quant.sf$", full.names = TRUE, recursive = TRUE)
 
-# Name each "sf.files" entry with the corresponding sample's name.
-names(sf.files) <- samples
+# Name each "sf_files" entry with the corresponding sample's name.
+names(sf_files) <- samples
 
 # Generate the gene to transcript mapping based on the input file
 # specified by "--gene2txmap" option:
-gene2tx.file <- opt$gene2txmap
-gene2tx.df <- readr::read_tsv(gene2tx.file, col_names = c("gene_id", "tx_name", "DROP"))
-gene2tx.df <- dplyr::select(gene2tx.df, -DROP)
+gene2tx_file <- opt$gene2txmap
+gene2tx_df <- readr::read_tsv(gene2tx_file, col_names = c("gene_id", "tx_name"))
 # Reorder columns into: transcript, gene (which is required for tximport)
-tx2gene <- gene2tx.df[, c("tx_name", "gene_id")]
+tx2gene <- gene2tx_df[, c("tx_name", "gene_id")]
 
 # Run tximport and save RDS
-txi.out <- tximport::tximport(files = sf.files,
-                              type = "salmon",
-                              txOut = TRUE,
-                              tx2gene = tx2gene,
-                              countsFromAbundance = "no")
-rds_filename <- file.path(exp.directory, "txi_out.RDS")
-saveRDS(txi.out, file = rds_filename)
+txi <- tximport::tximport(files = sf_files,
+                          type = "salmon",
+                          txOut = FALSE,
+                          tx2gene = tx2gene,
+                          countsFromAbundance = "no")
+rds_filename <- file.path(exp_dir, "txi_out.RDS")
+saveRDS(txi, file = rds_filename)
 
 # Run tximport::summarizeToGene() and save length-scaled
 # TPM (transcripts per million) file.
-txi.sum <- tximport::summarizeToGene(txi = txi.out,
-                                     tx2gene = tx2gene,
-                                     countsFromAbundance = "lengthScaledTPM")
+txi_length_scaled <- tximport::tximport(files = sf_files,
+                                        type = "salmon",
+                                        tx2gene = tx2gene,
+                                        countsFromAbundance = "lengthScaledTPM")
 # As data.frame, write to file
-lstpm.df <- as.data.frame(txi.sum$counts)
-lstpm.df <- cbind(rownames(lstpm.df), lstpm.df)
-colnames(lstpm.df)[1] <- "Gene"
-tpm_filename <- file.path(exp.directory, "gene_lengthScaledTPM.tsv")
-readr::write_tsv(lstpm.df, path = tpm_filename)
+lstpm_df <- as.data.frame(txi_length_scaled$counts)
+lstpm_df <- tibble::rownames_to_column(lstpm_df, var = "Gene")
+tpm_filename <- file.path(exp_dir, "gene_lengthScaledTPM.tsv")
+readr::write_tsv(lstpm_df, path = tpm_filename)
