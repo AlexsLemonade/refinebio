339b51cbf12ba10f6c27c85b3ea9c8d762ebfa75
Rich Jones <miserlou@gmail.com>
2018-05-25 15:06:17 -0400

    also, use the correct image. didnt catch this because i still had the old one when running local tests

--- api/run_tests.sh
+++ api/run_tests.sh
@@ -24,4 +24,4 @@ docker run \
        --add-host=nomad:$HOST_IP \
        --env-file api/environments/test \
        --link drdb:postgres \
-       -it ccdl/dr_api bash -c "$(run_tests_with_coverage $@)"
+       -it ccdl/dr_api_local bash -c "$(run_tests_with_coverage $@)"
