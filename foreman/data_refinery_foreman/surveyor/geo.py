b6c1570a4634ab82a6102dd018b99935f55fef3f
Kurt Wheeler <kurt.wheeler91@gmail.com>
2018-06-01 16:22:44 -0400

    Sets the title of experiment objects in GEO surveyor instead of the name. (#300)

--- foreman/data_refinery_foreman/surveyor/geo.py
+++ foreman/data_refinery_foreman/surveyor/geo.py
@@ -79,7 +79,7 @@ class GeoSurveyor(ExternalSourceSurveyor):
             experiment_object.accession_code = experiment_accession_code
             experiment_object.source_url = "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=" + experiment_accession_code
             experiment_object.source_database = "GEO"
-            experiment_object.name = gse.metadata.get('title', [''])[0]
+            experiment_object.title = gse.metadata.get('title', [''])[0]
             experiment_object.description = gse.metadata.get('summary', [''])[0]
             
             # Related: https://github.com/AlexsLemonade/refinebio/issues/222
