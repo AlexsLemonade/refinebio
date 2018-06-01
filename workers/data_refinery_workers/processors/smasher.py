1f95ad9bf62abdfc4064eb9d50c1fa2da8bee7af
Rich Jones <miserlou@gmail.com>
2018-06-01 15:48:22 -0400

    adds fail support to smasher - todo: send fail emails? fail display page?

--- workers/data_refinery_workers/processors/smasher.py
+++ workers/data_refinery_workers/processors/smasher.py
@@ -53,84 +53,95 @@ def _smash(job_context: Dict) -> Dict:
         Transpose again such that samples are columns and genes are rows
     """
 
-    # Prepare the output directory
-    smash_path = "/home/user/data_store/smashed/" + str(job_context["dataset"].pk) + "/"
-    os.makedirs(smash_path, exist_ok=True)
+    try:
+        # Prepare the output directory
+        smash_path = "/home/user/data_store/smashed/" + str(job_context["dataset"].pk) + "/"
+        os.makedirs(smash_path, exist_ok=True)
 
-    scalers = {
-        'MINMAX': preprocessing.MinMaxScaler,
-        'STANDARD': preprocessing.StandardScaler,
-        'ROBUST': preprocessing.RobustScaler,
-    }
+        scalers = {
+            'MINMAX': preprocessing.MinMaxScaler,
+            'STANDARD': preprocessing.StandardScaler,
+            'ROBUST': preprocessing.RobustScaler,
+        }
 
-    num_samples = 0
-    # Smash all of the sample sets
-    for key, input_files in job_context['input_files'].items():
+        num_samples = 0
+        # Smash all of the sample sets
+        for key, input_files in job_context['input_files'].items():
 
-        # Merge all the frames into one
-        all_frames = []
-        for computed_file in input_files:
-            data = pd.DataFrame.from_csv(str(computed_file.absolute_file_path), sep='\t', header=0)
-            all_frames.append(data)
-            num_samples = num_samples + 1
-        merged = all_frames[0]
-        i = 1
-        while i < len(all_frames):
-            merged = merged.merge(all_frames[i], left_index=True, right_index=True)
-            i = i + 1
+            # Merge all the frames into one
+            all_frames = []
+            for computed_file in input_files:
+                data = pd.DataFrame.from_csv(str(computed_file.absolute_file_path), sep='\t', header=0)
+                all_frames.append(data)
+                num_samples = num_samples + 1
+            merged = all_frames[0]
+            i = 1
+            while i < len(all_frames):
+                merged = merged.merge(all_frames[i], left_index=True, right_index=True)
+                i = i + 1
 
-        # Transpose before scaling
-        transposed = merged.transpose()
+            # Transpose before scaling
+            transposed = merged.transpose()
 
-        # Scaler
-        if job_context['dataset'].scale_by != "NONE":
-            scale_funtion = scalers[job_context['dataset'].scale_by]
-            scaler = scale_funtion(copy=True)
-            scaler.fit(transposed)
-            scaled = pd.DataFrame(  scaler.transform(transposed), 
-                                    index=transposed.index, 
-                                    columns=transposed.columns
-                                )
-            # Untranspose
-            untransposed = scaled.transpose()
-        else:
-            # Wheeeeeeeeeee
-            untransposed = transposed.transpose()
+            # Scaler
+            if job_context['dataset'].scale_by != "NONE":
+                scale_funtion = scalers[job_context['dataset'].scale_by]
+                scaler = scale_funtion(copy=True)
+                scaler.fit(transposed)
+                scaled = pd.DataFrame(  scaler.transform(transposed), 
+                                        index=transposed.index, 
+                                        columns=transposed.columns
+                                    )
+                # Untranspose
+                untransposed = scaled.transpose()
+            else:
+                # Wheeeeeeeeeee
+                untransposed = transposed.transpose()
 
-        # Write to temp file with dataset UUID in filename.
-        outfile = smash_path + key + ".tsv"
-        untransposed.to_csv(outfile, sep='\t', encoding='utf-8')
+            # Write to temp file with dataset UUID in filename.
+            outfile = smash_path + key + ".tsv"
+            untransposed.to_csv(outfile, sep='\t', encoding='utf-8')
 
-    # Copy LICENSE.txt and README.md files
-    shutil.copy("README_DATASET.md", smash_path + "README.md")
-    shutil.copy("LICENSE_DATASET.txt", smash_path + "LICENSE.TXT")
+        # Copy LICENSE.txt and README.md files
+        shutil.copy("README_DATASET.md", smash_path + "README.md")
+        shutil.copy("LICENSE_DATASET.txt", smash_path + "LICENSE.TXT")
 
-    # Create metadata file.
-    metadata = {}
-    metadata['files'] = os.listdir(smash_path)
-    metadata['num_samples'] = num_samples
-    metadata['num_experiments'] = job_context["experiments"].count()
-    metadata['aggregate_by'] = job_context["dataset"].aggregate_by
-    metadata['scale_by'] = job_context["dataset"].scale_by
+        # Create metadata file.
+        metadata = {}
+        metadata['files'] = os.listdir(smash_path)
+        metadata['num_samples'] = num_samples
+        metadata['num_experiments'] = job_context["experiments"].count()
+        metadata['aggregate_by'] = job_context["dataset"].aggregate_by
+        metadata['scale_by'] = job_context["dataset"].scale_by
 
-    samples = {}
-    for sample in job_context["dataset"].get_samples():
-        samples[sample.title] = sample.to_metadata_dict()
-    metadata['samples'] = samples
+        samples = {}
+        for sample in job_context["dataset"].get_samples():
+            samples[sample.title] = sample.to_metadata_dict()
+        metadata['samples'] = samples
 
-    experiments = {}
-    for experiment in job_context["dataset"].get_experiments():
-        experiments[experiment.accession_code] = experiment.to_metadata_dict()
-    metadata['experiments'] = experiments
+        experiments = {}
+        for experiment in job_context["dataset"].get_experiments():
+            experiments[experiment.accession_code] = experiment.to_metadata_dict()
+        metadata['experiments'] = experiments
 
-    metadata['created_at'] = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')
-    with open(smash_path + 'metadata.json', 'w') as metadata_file: 
-        json.dump(metadata, metadata_file, indent=4, sort_keys=True)
+        metadata['created_at'] = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')
+        with open(smash_path + 'metadata.json', 'w') as metadata_file: 
+            json.dump(metadata, metadata_file, indent=4, sort_keys=True)
 
-    # Finally, compress all files into a zip
-    final_zip = smash_path + str(job_context['dataset'].id)
-    shutil.make_archive(final_zip, 'zip', smash_path)
-    job_context["output_file"] = final_zip + ".zip"
+        # Finally, compress all files into a zip
+        final_zip = smash_path + str(job_context['dataset'].id)
+        shutil.make_archive(final_zip, 'zip', smash_path)
+        job_context["output_file"] = final_zip + ".zip"
+    except Exception as e:
+        job_context['dataset'].success = False
+        job_context['dataset'].failure_reason = str(e)
+        job_context['dataset'].save()
+        job_context['success'] = False
+        job_context['failure_reason'] = str(e)
+        return job_context
+
+    job_context['dataset'].success = True
+    job_context['dataset'].save()
 
     return job_context
 
