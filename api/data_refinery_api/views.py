2fa13e06b752e2be02f169fe5a25c1138d74462d
Stephanie Liu <stephofliu@gmail.com>
2018-06-01 16:21:22 -0400

    count filters based on results of query

--- api/data_refinery_api/views.py
+++ api/data_refinery_api/views.py
@@ -22,8 +22,8 @@ from data_refinery_common.models import (
     ProcessorJob,
     Dataset
 )
-from data_refinery_api.serializers import ( 
-    ExperimentSerializer, 
+from data_refinery_api.serializers import (
+    ExperimentSerializer,
     DetailedExperimentSerializer,
     SampleSerializer,
     DetailedSampleSerializer,
@@ -96,7 +96,7 @@ class SearchAndFilter(generics.ListAPIView):
     filter_backends = (DjangoFilterBackend, filters.SearchFilter,)
     search_fields = ('title', '@description')
     filter_fields = ('has_publication', 'submitter_institution', 'technology', 'source_first_published')
-    
+
 
     def list(self, request, *args, **kwargs):
         """ Adds counts on certain filter fields to result JSON."""
@@ -107,18 +107,20 @@ class SearchAndFilter(generics.ListAPIView):
         response.data['filters']['publication'] = {}
         response.data['filters']['organism'] = {}
 
-        techs = self.get_queryset().values('technology').annotate(Count('technology', unique=True))
+        qs = self.filter_queryset(self.get_queryset())
+
+        techs = qs.values('technology').annotate(Count('technology', unique=True))
         for tech in techs:
             response.data['filters']['technology'][tech['technology']] = tech['technology__count']
 
-        pubs = self.get_queryset().values('has_publication').annotate(Count('has_publication', unique=True))
+        pubs = qs.values('has_publication').annotate(Count('has_publication', unique=True))
         for pub in pubs:
             if pub['has_publication']:
                 response.data['filters']['publication']['has_publication'] = pub['has_publication__count']
-        if 'has_publication' not in response.data['filters']:
+        if 'has_publication' not in response.data['filters']['publication']:
             response.data['filters']['publication']['has_publication'] = 0
 
-        organisms = self.get_queryset().values('organisms__name').annotate(Count('organisms__name', unique=True))
+        organisms = qs.values('organisms__name').annotate(Count('organisms__name', unique=True))
         for organism in organisms:
             response.data['filters']['organism'][organism['organisms__name']] = organism['organisms__name__count']
 
