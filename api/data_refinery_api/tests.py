a0881c4c8c0918cd7a363fd8c64c97230b12d54c
Kurt Wheeler <kurt.wheeler91@gmail.com>
2018-06-06 15:29:20 -0400

    Actually re-add technology back to API tests, undoes some of the autoformatting I let through.

--- api/data_refinery_api/tests.py
+++ api/data_refinery_api/tests.py
@@ -217,6 +217,7 @@ class SanityTestAllEndpoints(APITestCase):
                                         for i in range(3)) + str(random.randint(0, 1000))[:64]
             ex.title = " ".join(random.choice(words) for i in range(10))
             ex.description = " ".join(random.choice(words) for i in range(100))
+            ex.technology = random.choice(["RNA-SEQ", "MICROARRAY"])
             ex.submitter_institution = random.choice(["Funkytown", "Monkeytown"])
             experiments.append(ex)
 
@@ -224,6 +225,7 @@ class SanityTestAllEndpoints(APITestCase):
         ex.accession_code = "FINDME"
         ex.title = "THISWILLBEINASEARCHRESULT"
         ex.description = "SOWILLTHIS"
+        ex.technology = "MICROARRAY"
         ex.submitter_institution = "Funkytown"
         experiments.append(ex)
 
@@ -231,6 +233,7 @@ class SanityTestAllEndpoints(APITestCase):
         ex.accession_code = "FINDME2"
         ex.title = "THISWILLBEINASEARCHRESULT"
         ex.description = "SOWILLTHIS"
+        ex.technology = "RNA-SEQ"
         ex.submitter_institution = "Funkytown"
         experiments.append(ex)
 
@@ -245,8 +248,9 @@ class SanityTestAllEndpoints(APITestCase):
         self.assertEqual(response.json()['count'], 2)
 
         # Test search and filter
-        response = self.client.get(
-            reverse('search'), {'search': 'THISWILLBEINASEARCHRESULT', 'technology': 'MICROARRAY'})
+        response = self.client.get(reverse('search'),
+                                   {'search': 'THISWILLBEINASEARCHRESULT',
+                                    'technology': 'MICROARRAY'})
         self.assertEqual(response.json()['count'], 1)
         self.assertEqual(response.json()['results'][0]['accession_code'], 'FINDME')
 
@@ -254,8 +258,9 @@ class SanityTestAllEndpoints(APITestCase):
 
         # Good
         jdata = json.dumps({'data': {"A": ["B"]}})
-        response = self.client.post(
-            reverse('create_dataset'), jdata, content_type="application/json")
+        response = self.client.post(reverse('create_dataset'),
+                                    jdata,
+                                    content_type="application/json")
 
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response.json()['data'], json.loads(jdata)['data'])
@@ -268,8 +273,9 @@ class SanityTestAllEndpoints(APITestCase):
 
         # Update
         jdata = json.dumps({'data': {"A": ["C"]}})
-        response = self.client.put(
-            reverse('dataset', kwargs={'id': good_id}), jdata, content_type="application/json")
+        response = self.client.put(reverse('dataset', kwargs={'id': good_id}),
+                                   jdata,
+                                   content_type="application/json")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json()['id'], good_id)
         self.assertEqual(response.json()['data'], json.loads(jdata)['data'])
@@ -280,12 +286,14 @@ class SanityTestAllEndpoints(APITestCase):
         dataset.is_processing = True
         dataset.save()
         jdata = json.dumps({'data': {"A": ["D"]}})
-        response = self.client.put(
-            reverse('dataset', kwargs={'id': good_id}), jdata, content_type="application/json")
+        response = self.client.put(reverse('dataset', kwargs={'id': good_id}),
+                                   jdata,
+                                   content_type="application/json")
         self.assertNotEqual(response.json()['data']["A"], ["D"])
 
         # Bad
         jdata = json.dumps({'data': 123})
-        response = self.client.post(
-            reverse('create_dataset'), jdata, content_type="application/json")
+        response = self.client.post(reverse('create_dataset'),
+                                    jdata,
+                                    content_type="application/json")
         self.assertEqual(response.status_code, 400)
