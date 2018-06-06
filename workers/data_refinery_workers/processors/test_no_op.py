7577120fa9f24293cfc53b78e298fd71825ba463
Rich Jones <miserlou@gmail.com>
2018-06-06 15:06:30 -0400

    need to merge

--- workers/data_refinery_workers/processors/test_no_op.py
+++ workers/data_refinery_workers/processors/test_no_op.py
@@ -36,3 +36,34 @@ class NOOPTestCase(TestCase):
         """ """
         job = prepare_job()
         no_op.no_op_processor(job.pk)
+
+    @tag('no_op')
+    def test_convert_simple_pcl(self):
+        """ """
+        job = prepare_job()
+
+        pj = ProcessorJob()
+        pj.pipeline_applied = "NO_OP"
+        pj.save()
+
+        # ID_REF    VALUE
+        og_file = OriginalFile()
+        og_file.source_filename = "ftp://ftp.ebi.ac.uk/pub/databases/microarray/data/experiment/GEOD/E-GEOD-59079/E-GEOD-59079.processed.1.zip"
+        og_file.filename = "GSM1426279_sample_table.txt"
+        og_file.absolute_file_path = "/home/user/data_store/raw/TEST/NOOP/GSM1426279_sample_table.txt"
+        og_file.save()
+
+        assoc1 = ProcessorJobOriginalFileAssociation()
+        assoc1.original_file = og_file
+        assoc1.processor_job = pj
+        assoc1.save()
+
+        no_op.no_op_processor(job.pk)
+
+        # No header - ex 
+        # AFFX-BioB-3_at  0.74218756
+        og_file = OriginalFile()
+        og_file.source_filename = "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE10188&format=file"
+        og_file.filename = "GSM269747-tbl-1.txt"
+        og_file.absolute_file_path = "/home/user/data_store/raw/TEST/NOOP/GSM269747-tbl-1.txt"
+        og_file.save()
