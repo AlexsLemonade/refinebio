3fe2d6a050766fcd6e51d3902db7f8d15d4becb7
Kurt Wheeler <kurt.wheeler91@gmail.com>
2018-06-11 12:12:32 -0400

    Installs nomad and terraform onto CI machines in a safe way.

--- .circleci/run_terraform.sh
+++ .circleci/run_terraform.sh
@@ -1,13 +1,56 @@
+# Import Hashicorps' Key.
+curl https://keybase.io/hashicorp/pgp_keys.asc | gpg --import
+
+
 # Install terraform and nomad
 cd
-wget https://releases.hashicorp.com/terraform/0.11.3/terraform_0.11.3_linux_amd64.zip
-unzip terraform_0.11.3_linux_amd64.zip
+TERRAFORM_VERSION=0.11.7
+wget https://releases.hashicorp.com/terraform/$TERRAFORM_VERSION/terraform_$TERRAFORM_VERSION_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/$TERRAFORM_VERSION/terraform_$TERRAFORM_VERSION_SHA256SUMS
+wget https://releases.hashicorp.com/terraform/$TERRAFORM_VERSION/terraform_$TERRAFORM_VERSION_SHA256SUMS.sig
+
+
+# Verify the signature file is untampered.
+gpg_ok=$(gpg --verify terraform_$TERRAFORM_VERSION_SHA256SUMS.sig terraform_$TERRAFORM_VERSION_SHA256SUMS |& grep Good)
+if [[ "$gpg_ok" = "" ]]; then
+    echo "Could not verify the signature from HashiCorp Security <security@hashicorp.com>."
+    exit 1
+fi
+
+# Verify the SHASUM matches the binary.
+shasum_ok=$(sha256sum -c terraform_$TERRAFORM_VERSION_SHA256SUMS |& grep OK)
+if [[ "$shasum_ok" = "" ]]; then
+    echo "Could not verify the Terraform checksum provided by Hashicorp."
+    exit 1
+fi
+
+unzip terraform_$TERRAFORM_VERSION_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
 
 sudo apt-get update
 sudo apt-get install lxc -y  # Install lxc, which is required by nomad
-wget https://releases.hashicorp.com/nomad/0.7.1/nomad_0.7.1_linux_amd64-lxc.zip
-unzip nomad_0.7.1_linux_amd64-lxc.zip
+
+NOMAD_VERSION=0.8.3
+wget https://releases.hashicorp.com/nomad/$NOMAD_VERSION/nomad_$NOMAD_VERSION_linux_amd64.zip
+wget https://releases.hashicorp.com/nomad/$NOMAD_VERSION/nomad_$NOMAD_VERSION_SHA256SUMS
+wget https://releases.hashicorp.com/nomad/$NOMAD_VERSION/nomad_$NOMAD_VERSION_SHA256SUMS.sig
+
+
+# Verify the signature file is untampered.
+gpg_ok=$(gpg --verify terraform_$NOMAD_VERSION_SHA256SUMS.sig nomad_$NOMAD_VERSION_SHA256SUMS |& grep Good)
+if [[ "$gpg_ok" = "" ]]; then
+    echo "Could not verify the signature from HashiCorp Security <security@hashicorp.com>."
+    exit 1
+fi
+
+# Verify the SHASUM matches the binary.
+shasum_ok=$(sha256sum -c nomad_$NOMAD_VERSION_SHA256SUMS |& grep OK)
+if [[ "$shasum_ok" = "" ]]; then
+    echo "Could not verify the Nomad checksum provided by Hashicorp."
+    exit 1
+fi
+
+unzip nomad_$NOMAD_VERSION_linux_amd64.zip
 sudo mv nomad /usr/local/bin/
 
 cd ~/refinebio/.circleci/s3_tfstate
