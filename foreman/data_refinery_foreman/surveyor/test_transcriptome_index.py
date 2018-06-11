496c651e4482293f87a8a663805f00d6f3cba609
Kurt Wheeler <kurt.wheeler91@gmail.com>
2018-06-11 10:48:30 -0400

    Use APIs directly in transcriptome index unit tests rather than mocking them out.

--- foreman/data_refinery_foreman/surveyor/test_transcriptome_index.py
+++ foreman/data_refinery_foreman/surveyor/test_transcriptome_index.py
@@ -26,39 +26,12 @@ class SurveyTestCase(TestCase):
         key_value_pair.save()
 
     @patch('data_refinery_foreman.surveyor.external_source.send_job')
-    @patch("data_refinery_foreman.surveyor.transcriptome_index.urllib.request.urlopen")
-    @patch("data_refinery_foreman.surveyor.transcriptome_index.requests.get")
-    def test_survey(self, mock_get, mock_urlopen, mock_send_job):
-        json_file_path = os.path.join(os.path.dirname(__file__), "test_transcriptome_species.json")
-        with open(json_file_path, "r") as json_file:
-            species_json = json.load(json_file)
-
-        # Insert the organisms into the database so the model doesn't call the
-        # taxonomy API to populate them.
-        for species in species_json:
-            # Account for the subtle difference between the API for
-            # the main Ensembl division and the API for the rest of
-            # them.
-            name_key = "common_name" if "common_name" in species else "name"
-            taxonomy_key = "taxonomy_id" if "taxonomy_id" in species else "taxon_id"
-            organism = Organism(name=species[name_key].upper(),
-                                taxonomy_id=species[taxonomy_key],
-                                is_scientific_name=True)
-            organism.save()
-
-        mock_get.return_value = Mock(ok=True)
-        mock_get.return_value.json.return_value = species_json
-
-        # There are two possible file locations. The correct one is
-        # determined by making a request to one to see if it
-        # exists. This URLError simulates it not existing.
-        mock_urlopen.side_effect = URLError("404 or something")
-
+    def test_survey(self, mock_send_job):
         surveyor = TranscriptomeIndexSurveyor(self.survey_job)
         surveyor.survey()
 
         downloader_jobs = DownloaderJob.objects.order_by("id").all()
-        self.assertEqual(downloader_jobs.count(), len(species_json))
+        self.assertEqual(downloader_jobs.count(), 53)
         send_job_calls = []
         for downloader_job in downloader_jobs:
             send_job_calls.append(
@@ -84,7 +57,7 @@ class SurveyTestCase(TestCase):
                                                 key="number_of_organisms",
                                                 value=1)
         key_value_pair.save()
-        
+
         key_value_pair = SurveyJobKeyValue(survey_job=survey_job,
                                                 key="organism_name",
                                                 value="Danio rerio")
--- foreman/data_refinery_foreman/surveyor/transcriptome_index.py
+++ foreman/data_refinery_foreman/surveyor/transcriptome_index.py
@@ -1,5 +1,4 @@
 import re
-import requests
 import urllib
 
 from abc import ABC
