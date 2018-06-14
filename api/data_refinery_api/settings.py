31eac6c4abe0abefe588bd8d9b42f1f97ddb61ba
Rich Jones <miserlou@gmail.com>
2018-06-14 11:15:06 -0400

    adds django-cors-headers

--- api/data_refinery_api/settings.py
+++ api/data_refinery_api/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'rest_framework_hstore',
     'coreapi',
     'django_filters',
+    'corsheaders',
     
     # Local
     'data_refinery_common',
@@ -57,6 +58,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
