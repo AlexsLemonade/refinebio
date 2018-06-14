96c5ae2cdd2f6d9c369472b2d1977c02d12c652e
Kurt Wheeler <kurt.wheeler91@gmail.com>
2018-06-14 12:45:24 -0400

    Kurtwheeler fix foreman (#314)
    * Reworks the foreman to work with the newer data models and the switch to Nomad. UNTESTED.
    
    * Reenable the foreman tests.
    
    * update send_jobs to reflect new function signature
    
    * job -> job.id

--- common/data_refinery_common/message_queue.py
+++ common/data_refinery_common/message_queue.py
@@ -18,13 +18,12 @@ NOMAD_DOWNLOADER_JOB = "DOWNLOADER"
 NONE_JOB_ERROR_TEMPLATE = "send_job was called with NONE job_type: {} for {} job {}"
 
 
-def send_job(job_type: Enum, job_id: int) -> None:
+def send_job(job_type: Enum, job) -> None:
     """Queues a worker job by sending a Nomad Job dispatch message.
 
     job_type must be a valid Enum for ProcessorPipelines or
     Downloaders as defined in data_refinery_common.job_lookup.
-    job_id must correspond to an existing ProcessorJob or
-    DownloaderJob record.
+    job must be an existing ProcessorJob or DownloaderJob record.
     """
     nomad_host = get_env_variable("NOMAD_HOST")
     nomad_port = get_env_variable("NOMAD_PORT", "4646")
@@ -60,11 +59,13 @@ def send_job(job_type: Enum, job_id: int) -> None:
     logger.info("Queuing %s nomad job to run job %s with id %d.",
                 nomad_job,
                 job_type.value,
-                job_id)
+                job.id)
     try:
-        nomad_client.job.dispatch_job(nomad_job, meta={"JOB_NAME": job_type.value,
-                                                       "JOB_ID": str(job_id)})
+        nomad_response = nomad_client.job.dispatch_job(nomad_job, meta={"JOB_NAME": job_type.value,
+                                                                        "JOB_ID": str(job.id)})
+        job.nomad_job_id = nomad_response["DispatchedJobID"]
+        job.save()
     except URLNotFoundNomadException:
         logger.error("Dispatching Nomad job of type %s to host %s and port %s failed.",
-                     job_type, nomad_host, nomad_port)
+                     job_type, nomad_host, nomad_port, job=str(job.id))
         raise
