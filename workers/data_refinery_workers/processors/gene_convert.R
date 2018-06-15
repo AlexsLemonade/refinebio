b02491f4fe1880769a7ee16086d27429a1427559
Rich Jones <miserlou@gmail.com>
2018-06-15 10:31:42 -0400

    use drply in gene convert

--- workers/data_refinery_workers/processors/gene_convert.R
+++ workers/data_refinery_workers/processors/gene_convert.R
@@ -5,12 +5,17 @@
 #
 ###
 
+# import magrittr pipe
+`%>%` <- dplyr::`%>%`
+
 #######################
 # The command interface!
 #######################
 
 library("optparse")
 library(data.table)
+library("dplyr")
+library("rlang")
 
 option_list = list(
   make_option(c("-p", "--platform"), type="character", default="", 
@@ -32,8 +37,8 @@ outFilePath <- opt$outputFile
 message("Loading...")
 source("http://bioconductor.org/biocLite.R")
 
-library(Biobase)
-library("AnnotationDbi")
+# library(Biobase)
+# library("AnnotationDbi")
 
 # Read the data file
 message("Reading data file...")
@@ -63,39 +68,56 @@ suppressWarnings(index_data <- fread(index_path,
 					showProgress=FALSE)
 				)
 
-# we'll call these the supported identifiers
-supported_ids <- c("PROBEID", "ENTREZID", "SYMBOL", "ENSEMBL", "UNIGENE")
-
-# message(index_data)
-# res <- select(index_data, keys=keys(index_data), columns=supported_ids)
-
 # find % overlap by column
 overlap <- apply(index_data, 2, function(x) length(intersect(data$ID_REF, x)) / length(data$ID_REF))
 
 # threshold here can be adjusted
-# if (any(overlap > 0.9)) {
-if (any(overlap > 0.6)) {
+if (any(overlap >= 0.5)) {
 	# take column name with highest overlap value 
   	# overlap is a named numeric vector
   	detected_id <- names(which.max(overlap))
 } else {
+	# Less than 50%? Get outta here!
   	stop("Not enough overlapping ids detected!")
 }
 
-# Replace the probe IDs with Ensembles
-i <- 0
-c <- which( colnames(index_data) == detected_id )
+message("Merging..")
+colnames(data)[1] <- detected_id
+#merged <- merge(data, index_data, by=detected_id, all.x=TRUE)
 
-for(m in data[, 1]){
-	data[i, 1] = index_data[i, c]
-	i <- i + 1;
-}
+# Old version - R has 'choose the first' behavior
+#out<-join(df1, df2, type="full")
+
+# Take 2
+# merged <- dplyr::inner_join(data, index_data, by=detected_id)
+# message(head(merged))
+
+converted_exprs <- index_data %>%  
+  dplyr::select(c("ENSEMBL", detected_id)) %>%  # sub "ENSEMBL" for "GENEID"
+  dplyr::inner_join(data, by = detected_id)
+
+converted_exprs <- converted_exprs %>%
+  dplyr::select(-rlang::UQ(rlang::sym(detected_id)))
+
+message(head(converted_exprs))
+
+# data <- merged[, c(3,2)]
+
+# message("Replacing..")
+# # Replace the probe IDs with Ensembles
+# i <- 0
+# c <- which( colnames(index_data) == detected_id )
+# ec <- which( colnames(index_data) == "ENSEMBL" )
+# for(m in data[, 1]){
+# 	data[i, 1] = index_data[i, ec]
+# 	i <- i + 1;
+# }
 
 # Remove all of the unmapped (NA) values (likely control probes?)
-data <- data[complete.cases(data), ]
+# data <- data[complete.cases(data), ]
 
 # Data here can have duplicate rows that need to be squished together (via mean/max/etc),
 # but this should be done at smash-time so that we can provide options on the squish method.
 
 # Save to output file
-write.table(data, outFilePath, row.names=FALSE, col.names=TRUE, quote=FALSE, sep="\t")
+write.table(converted_exprs, outFilePath, row.names=FALSE, col.names=TRUE, quote=FALSE, sep="\t")
