e7e79a738342c4e7c2a4617c946d5df6cf9365af
Will Vauclain <willvauclain.dev@gmail.com>
2018-06-18 16:09:33 -0400

    Added detection for Arch linux in ip address function

--- common.sh
+++ common.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 get_ip_address () {
-    if [ `uname` == "Linux" ]; then
-        echo $(dig +short myip.opendns.com @resolver1.opendns.com)
+    if [ -e /etc/arch-release ]; then
+        echo $(hostname -i | awk '{print $1; exit}')
+    elif [ `uname` == "Linux" ]; then
+        echo $(ip route get 8.8.8.8 | awk '{print $NF; exit}')
     elif [ `uname` == 'Darwin' ]; then # MacOS
         echo $(ifconfig | grep "inet " | grep -v 127.0.0.1 | cut -d\  -f2)
     fi
