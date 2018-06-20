c35bcac23f3e3d772de7b6ab1e9faeff2c729e16
Will Vauclain <willvauclain.dev@gmail.com>
2018-06-20 11:30:09 -0400

    Ensure nomad is running before starting tests

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -5,9 +5,17 @@
 # This can be done with:
 # sudo -E ./run_nomad.sh -e test
 # This should mirror what happens in the CircleCI config (.circleci/config).
-mkdir -p test_volume && chmod -R a+rw test_volume
-./update_models.sh
-./api/run_tests.sh
-./common/run_tests.sh
-./foreman/run_tests.sh
-./workers/run_tests.sh
+
+# Ensure that Nomad is running first
+if ps aux | grep test_nomad | grep -v grep; then
+    mkdir -p test_volume && chmod -R a+rw test_volume
+    ./update_models.sh
+    ./api/run_tests.sh
+    ./common/run_tests.sh
+    ./foreman/run_tests.sh
+    ./workers/run_tests.sh
+else
+    echo "You must start the nomad test environment first with" >&2
+    echo "'sudo -E ./run_nomad.sh -e test'" >&2
+    exit 1
+fi
