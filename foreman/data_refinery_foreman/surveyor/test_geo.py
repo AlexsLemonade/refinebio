581a4837216a9993795507306dea5abbfbd3c63b
Kurt Wheeler <kurt.wheeler91@gmail.com>
2018-07-27 10:54:42 -0400

    Tries a different GEO experiment in hopes that this one will be less prone to intermittant failures.

--- foreman/data_refinery_foreman/surveyor/test_geo.py
+++ foreman/data_refinery_foreman/surveyor/test_geo.py
@@ -38,15 +38,15 @@ class SurveyTestCase(TransactionTestCase):
 
         For an Illumina Microarray platform.
         """
-        self.prep_test("GSE19274")
+        self.prep_test("GSE11915")
 
         geo_surveyor = GeoSurveyor(self.survey_job)
         geo_surveyor.survey()
 
-        self.assertEqual(138, Sample.objects.all().count())
+        self.assertEqual(34, Sample.objects.all().count())
 
         sample_object = Sample.objects.first()
-        self.assertEqual(sample_object.platform_name, "Illumina human-6 v2.0 expression beadchip")
+        self.assertEqual(sample_object.platform_name, "[HG-U133A] Affymetrix Human Genome U133A Array")
         self.assertEqual(sample_object.platform_accession_code, "Illumina_Human-6_V2.0")
         self.assertEqual(sample_object.technology, "MICROARRAY")
 
