d456cf514fa62dbf46e4550a14d210175b8340b1
Rich Jones <miserlou@gmail.com>
2018-08-08 15:23:04 -0400

    adds in progress create_qn_target command

---
+++ workers/data_refinery_workers/processors/management/commands/create_qn_target.py
@@ -0,0 +1,75 @@
+import sys
+import requests
+
+from django.core.management.base import BaseCommand
+from django.db.models import Count
+
+from data_refinery_common.job_lookup import ProcessorPipeline
+from data_refinery_common.logging import get_and_configure_logger
+from data_refinery_common.message_queue import send_job
+from data_refinery_workers.processors import qn_reference, utils
+
+from data_refinery_common.models import (
+    Experiment,
+    Sample,
+    Organism,
+    ProcessorJob,
+    Dataset,
+    ProcessorJobDatasetAssociation,
+    ExperimentOrganismAssociation,
+    OrganismIndex,
+    ExperimentSampleAssociation
+)
+
+logger = get_and_configure_logger(__name__)
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--organism",
+            type=str,
+            help=("Name of organism"))
+
+    def handle(self, *args, **options):
+        """ 
+        """
+
+        if options["organism"] is None:
+            logger.error("You must specify an organism")
+            sys.exit(1) 
+
+        organism = Organism.get_object_for_name(options["organism"].upper())
+        # samples = Sample.processed_objects.filter(organism=organism)
+        # if samples.count() == 0:
+        #     logger.error("No processed samples for that organism.")
+        #     sys.exit(1) 
+
+        # platform_counts = Sample.processed_objects.objects.values('platform_name').annotate(dcount=Count('platform_name'))
+        platform_counts = Sample.objects.values('platform_accession_code').annotate(dcount=Count('platform_accession_code')).order_by('-dcount')
+        biggest_platform = platform_counts[0]['platform_accession_code']
+        sample_codes_results = Sample.objects.filter(platform_accession_code=biggest_platform).values('accession_code')
+        sample_codes = [res['accession_code'] for res in sample_codes_results]
+
+        dataset = Dataset()
+        dataset.data = {organism.name + '_(' + biggest_platform + ')': sample_codes}
+        dataset.aggregate_by = "ALL"
+        dataset.scale_by = "NONE"
+        dataset.save()
+
+        job = ProcessorJob()
+        job.pipeline_applied = "QN_REFERENCE"
+        job.save()
+
+        pjda = ProcessorJobDatasetAssociation()
+        pjda.processor_job = job
+        pjda.dataset = dataset
+        pjda.save()
+
+        final_context = qn_reference.create_qn_reference(job.pk)
+
+        if final_context['success']:
+            sys.exit(0)
+        else:
+            print(":(")
+            sys.exit(-1)
