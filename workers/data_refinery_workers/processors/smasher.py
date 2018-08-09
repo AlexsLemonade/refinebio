6e9bbc6e6c81647eacc6da3a2d7ee117c68f9592
Rich Jones <miserlou@gmail.com>
2018-08-09 17:20:01 -0400

    should work but doesnt

--- workers/data_refinery_workers/processors/smasher.py
+++ workers/data_refinery_workers/processors/smasher.py
@@ -195,7 +195,7 @@ def _smash(job_context: Dict) -> Dict:
                 qn_target = utils.get_most_recent_qn_target_for_organism(organism)
                 qn_target_path = qn_target.sync_from_s3()
 
-                qn_target_frame = pd.read_csv(qn_target_path, sep='\t', header=0, index_col=0, error_bad_lines=False)
+                qn_target_frame = pd.read_csv(qn_target_path, sep='\t', header=None, index_col=None, error_bad_lines=False)
 
                 import rpy2
                 from rpy2.robjects import pandas2ri
@@ -203,8 +203,14 @@ def _smash(job_context: Dict) -> Dict:
 
                 from rpy2.robjects.packages import importr
                 preprocessCore = importr('preprocessCore')
-                preprocessCore.normalize_quantiles(m)
 
+                import pdb
+                pdb.set_trace()
+
+                r_target_matrix = rpy2.robjects.r.matrix(qn_target_frame)
+                r_merged_matrix = rpy2.robjects.r.matrix(merged)
+
+                reso = preprocessCore.normalize_quantiles_use_target(r_merged_matrix, r_target_matrix)
 
                 import pdb
                 pdb.set_trace()
