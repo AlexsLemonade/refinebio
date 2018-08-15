e3049567973c9531601765b9b32e9bffd80f6b17
Rich Jones <miserlou@gmail.com>
2018-08-15 17:32:33 -0400

    more -y

--- infrastructure/nomad-configuration/pg-bouncer-instance-user-data.tpl.sh
+++ infrastructure/nomad-configuration/pg-bouncer-instance-user-data.tpl.sh
@@ -19,7 +19,7 @@ cd /home/ubuntu
 
 apt-get -y update
 sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key -y add -
 apt-get -y update
 apt-get -y install pgbouncer
 
