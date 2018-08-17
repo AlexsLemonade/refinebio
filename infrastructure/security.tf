5dd61177fba33e75b6fefd3310fcc0205f481c3d
Rich Jones <miserlou@gmail.com>
2018-08-17 14:39:03 -0400

    shorten password below stupid limit

--- infrastructure/security.tf
+++ infrastructure/security.tf
@@ -295,7 +295,7 @@ resource "aws_security_group_rule" "data_refinery_pg_api_icmp" {
 resource "aws_security_group_rule" "data_refinery_pg_outbound" {
   type = "egress"
   from_port = 0
-  to_port = 0
+  to_port = 65535
   protocol = "all"
   cidr_blocks = ["0.0.0.0/0"]
   ipv6_cidr_blocks = ["::/0"]
