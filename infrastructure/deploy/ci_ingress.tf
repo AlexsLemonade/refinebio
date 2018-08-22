8c404460d8095e0f3540b9d92d85cad8624dd472
Kurt Wheeler <kurt.wheeler91@gmail.com>
2018-08-22 15:22:28 -0400

    Fixes pgbouncer merge woes. Makes transcriptome bucket public-read. Makes all buckets removable by terraform. (#545)

--- infrastructure/deploy/ci_ingress.tf
+++ infrastructure/deploy/ci_ingress.tf
@@ -9,8 +9,8 @@ resource "aws_security_group_rule" "data_refinery_ci_nomad" {
 
 resource "aws_security_group_rule" "data_refinery_ci_postgres" {
   type = "ingress"
-  from_port = 5432
-  to_port = 5432
+  from_port = "${var.database_hidden_port}"
+  to_port = "${var.database_hidden_port}"
   protocol = "tcp"
   cidr_blocks = ["${var.host_ip}/32"]
   security_group_id = "${aws_security_group.data_refinery_db.id}"
