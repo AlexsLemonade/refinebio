a0bf0400ecc87f3ceebf93f21c4b8dcd9c9ae2ca
Rich Jones <miserlou@gmail.com>
2018-08-24 09:47:16 -0400

    rm extra comma

--- infrastructure/users.tf
+++ infrastructure/users.tf
@@ -71,7 +71,7 @@ resource "aws_iam_user_policy" "data_refinery_user_worker_policy" {
             "Effect": "Allow",
             "Action":[
               "ec2:DescribeVolumes",
-              "ec2:AttachVolume",
+              "ec2:AttachVolume"
             ],
             "Resource": "*"
         }
