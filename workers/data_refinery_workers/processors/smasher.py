615d533c1d56cdfb427ef110114da8f2f0b1ce9e
Kurt Wheeler <kurt.wheeler91@gmail.com>
2018-09-26 12:30:15 -0400

    Add comment explaining pvalue change.

--- workers/data_refinery_workers/processors/smasher.py
+++ workers/data_refinery_workers/processors/smasher.py
@@ -566,6 +566,9 @@ def _smash(job_context: Dict) -> Dict:
                             statistic = ks_res.rx('statistic')[0][0]
                             pvalue = ks_res.rx('p.value')[0][0]
 
+                            # We're unsure of how strigent to be about
+                            # the pvalue just yet, so we're extra lax
+                            # rather than failing tons of tests. This may need tuning.
                             if statistic > 0.001 or pvalue >= 0.8:
                                 raise Exception("Failed Kolmogorov Smirnov test! Stat: " +
                                                 str(statistic) + ", PVal: " + str(pvalue))
