c27593c36a8ec2b57f6916fed50abca80948e70e
Rich Jones <miserlou@gmail.com>
2018-10-10 14:06:06 -0400

    init janitor

--- workers/data_refinery_workers/processors/utils.py
+++ workers/data_refinery_workers/processors/utils.py
@@ -76,50 +76,56 @@ def start_job(job_context: Dict):
 
     logger.debug("Starting processor Job.", processor_job=job.id, pipeline=job.pipeline_applied)
 
-    # Some jobs take OriginalFiles, other take Datasets
-    if job.pipeline_applied not in ["SMASHER", "QN_REFERENCE"]:
-        relations = ProcessorJobOriginalFileAssociation.objects.filter(processor_job=job)
-        original_files = OriginalFile.objects.filter(id__in=relations.values('original_file_id'))
+    # Janitors have no requirement
+    if job.pipeline_applied not in ["JANITOR"]:
+        # Some jobs take OriginalFiles, other take Datasets
+        if job.pipeline_applied not in ["SMASHER", "QN_REFERENCE"]:
+            relations = ProcessorJobOriginalFileAssociation.objects.filter(processor_job=job)
+            original_files = OriginalFile.objects.filter(id__in=relations.values('original_file_id'))
 
-        if len(original_files) == 0:
-            logger.error("No files found.", processor_job=job.id)
-            job_context["success"] = False
-            job.failure_reason = "No files were found for the job."
-            return job_context
+            if len(original_files) == 0:
+                logger.error("No files found.", processor_job=job.id)
+                job_context["success"] = False
+                job.failure_reason = "No files were found for the job."
+                return job_context
 
-        job_context["original_files"] = original_files
-        original_file = job_context['original_files'][0]
-        assocs = OriginalFileSampleAssociation.objects.filter(original_file=original_file)
-        samples = Sample.objects.filter(id__in=assocs.values('sample_id')).distinct()
-        job_context['samples'] = samples
-        job_context["computed_files"] = []
+            job_context["original_files"] = original_files
+            original_file = job_context['original_files'][0]
+            assocs = OriginalFileSampleAssociation.objects.filter(original_file=original_file)
+            samples = Sample.objects.filter(id__in=assocs.values('sample_id')).distinct()
+            job_context['samples'] = samples
+            job_context["computed_files"] = []
 
+        else:
+            relations = ProcessorJobDatasetAssociation.objects.filter(processor_job=job)
+
+            # This should never be more than one!
+            if relations.count() > 1:
+                failure_reason = "More than one dataset for processor job!"
+                logger.error(failure_reason, processor_job=job.id)
+                job_context["success"] = False
+                job.failure_reason = failure_reason
+                return job_context
+            elif relations.count() == 0:
+                failure_reason = "No datasets found for processor job!"
+                logger.error(failure_reason, processor_job=job.id)
+                job_context["success"] = False
+                job.failure_reason = failure_reason
+                return job_context
+
+            dataset = Dataset.objects.get(id=relations[0].dataset_id)
+            dataset.is_processing = True
+            dataset.save()
+
+            # Get the samples to smash
+            job_context["dataset"] = dataset
+            job_context["samples"] = dataset.get_aggregated_samples()
+            job_context["experiments"] = dataset.get_experiments()
+
+            # Just in case
+            job_context["original_files"] = []
+            job_context["computed_files"] = []
     else:
-        relations = ProcessorJobDatasetAssociation.objects.filter(processor_job=job)
-
-        # This should never be more than one!
-        if relations.count() > 1:
-            failure_reason = "More than one dataset for processor job!"
-            logger.error(failure_reason, processor_job=job.id)
-            job_context["success"] = False
-            job.failure_reason = failure_reason
-            return job_context
-        elif relations.count() == 0:
-            failure_reason = "No datasets found for processor job!"
-            logger.error(failure_reason, processor_job=job.id)
-            job_context["success"] = False
-            job.failure_reason = failure_reason
-            return job_context
-
-        dataset = Dataset.objects.get(id=relations[0].dataset_id)
-        dataset.is_processing = True
-        dataset.save()
-
-        # Get the samples to smash
-        job_context["dataset"] = dataset
-        job_context["samples"] = dataset.get_aggregated_samples()
-        job_context["experiments"] = dataset.get_experiments()
-
         # Just in case
         job_context["original_files"] = []
         job_context["computed_files"] = []
@@ -150,7 +156,7 @@ def end_job(job_context: Dict, abort=False):
 
             if mark_as_processed:
                 # This handles most of our cases
-                for sample in job_context["samples"]:
+                for sample in job_context.get("samples", []):
                     sample.is_processed = True
                     sample.save()
 
@@ -285,6 +291,7 @@ class PipelineEnum(Enum):
     SMASHER = "Smasher"
     TX_INDEX = "Transcriptome Index"
     QN_REFERENCE = "Quantile Normalization Reference"
+    JANITOR = "Janitor"
 
 
 @unique
