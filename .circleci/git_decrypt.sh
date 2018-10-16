62ca60c7d0614b5ee349290748aa52dd66219852
Kurt Wheeler <kurt.wheeler91@gmail.com>
2018-10-16 12:11:51 -0400

    Install git-crypt during instance startup rather than every deploy.

--- .circleci/git_decrypt.sh
+++ .circleci/git_decrypt.sh
@@ -1,10 +1,3 @@
-# Install git-crypt
-cd
-git clone https://github.com/AGWA/git-crypt.git
-cd git-crypt
-make
-sudo make install
-
 # Unlock encrypted files
 cd ~/refinebio/.circleci
 openssl aes-256-cbc -md md5 -d -in exported.key.enc -out $KEY_FILENAME -k $OPENSSL_KEY
