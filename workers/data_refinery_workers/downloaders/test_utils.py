8135b57c0352b561650d6e3fb79d0c5de9f09eb3
Kurt Wheeler <kurt.wheeler91@gmail.com>
2018-10-23 11:18:16 -0400

    Add tag to downloaders test so it is actually run.

--- workers/data_refinery_workers/downloaders/test_utils.py
+++ workers/data_refinery_workers/downloaders/test_utils.py
@@ -8,6 +8,7 @@ from urllib.error import URLError
 from data_refinery_workers.downloaders import utils
 
 class UtilsTestCase(TestCase):
+    @tag('downloaders')
     def test_no_jobs_to_create(self):
         """Make sure this function doesn't raise an exception with no files."""
         create_processor_job_for_original_files([])
