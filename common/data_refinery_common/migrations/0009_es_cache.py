488da9d7e4bb3c29bef42f3cfd749c11163ef00e
Rich Jones <miserlou@gmail.com>
2019-01-16 17:05:02 -0500

    dont use model methods during migration

--- common/data_refinery_common/migrations/0009_es_cache.py
+++ common/data_refinery_common/migrations/0009_es_cache.py
@@ -9,9 +9,28 @@ def update_cached_values(apps, schema_editor):
 
     Experiment = apps.get_model('data_refinery_common', 'Experiment')
     for experiment in Experiment.objects.all():
-        experiment.update_num_samples()
-        experiment.update_sample_metadata_fields()
-        experiment.update_organism_names()
+        # Model methods can't be used during migrations. :(
+        # https://stackoverflow.com/a/37685925/1135467
+
+        # via experiment.update_num_samples
+        experiment.num_total_samples = experiment.samples.count()
+        experiment.num_processed_samples = experiment.samples.filter(is_processed=True).count()
+
+        # via experiment.update_sample_metadata_fields
+        fields = []
+        possible_fields = ['sex', 'age', 'specimen_part', 'genotype', 'disease', 'disease_stage',
+                           'cell_line', 'treatment', 'race', 'subject', 'compound', 'time']
+        samples = experiment.samples.all()
+        for field in possible_fields:
+            for sample in samples:
+                if getattr(sample, field) != None and getattr(sample, field) != '':
+                    fields.append(field)
+                    break
+
+        experiment.sample_metadata_fields = fields
+
+        # via experiment.update_organism_names
+        experiment.organism_names = list(set([organism.name for organism in self.organisms.all()]))
         experiment.save()
 
 class Migration(migrations.Migration):
