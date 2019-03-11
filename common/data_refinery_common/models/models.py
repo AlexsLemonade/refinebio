002cbd98247f75453c78660c7912762e093be001
Kurt Wheeler <kurt.wheeler91@gmail.com>
2019-03-11 15:09:24 -0400

    Looks like I finally cast the incatation correctly.

--- common/data_refinery_common/models/models.py
+++ common/data_refinery_common/models/models.py
@@ -956,13 +956,14 @@ class ComputedFile(models.Model):
 
     def create_download_url(self):
         """ Create a temporary URL from which the file can be downloaded."""
-        if settings.RUNNING_IN_CLOUD and self.s3_bucket and self.s3_key:
-            return s3.generate_presigned_url(
+        if self.s3_bucket and self.s3_key:
+            return S3.generate_presigned_url(
                 ClientMethod='get_object',
                 Params={
                     'Bucket': self.s3_bucket,
                     'Key': self.s3_key
-                }
+                },
+                ExpiresIn=(60 * 60 * 24) # 1 day in seconds.
             )
         else:
             return None
