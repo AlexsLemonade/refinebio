8043165c9ecc08a26a2e20d2312c5b21b9dbc720
Kurt Wheeler <kurt.wheeler91@gmail.com>
2019-04-25 15:50:30 -0400

    I forgot an incrementer?!?!?!?

--- foreman/data_refinery_foreman/foreman/main.py
+++ foreman/data_refinery_foreman/foreman/main.py
@@ -618,6 +618,7 @@ def retry_lost_downloader_jobs() -> None:
                     # The job never got put in the Nomad queue, no
                     # need to recreate it, we just gotta queue it up!
                     send_job(Downloaders[job.downloader_task], job=job, is_dispatch=True)
+                    jobs_queued_from_this_page += 1
             except socket.timeout:
                 logger.info("Timeout connecting to Nomad - is Nomad down?", job_id=job.id)
             except URLNotFoundNomadException:
