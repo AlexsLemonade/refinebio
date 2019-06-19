332513fc505cdc3e0b4c7ced617b928d422e22b2
Ariel <arielsvn@gmail.com>
2019-06-19 16:08:15 -0400

    keep num_processed_samples in the search filters

--- api/data_refinery_api/views.py
+++ api/data_refinery_api/views.py
@@ -229,6 +229,14 @@ class ExperimentDocumentView(DocumentViewSet):
         'has_publication': 'has_publication',
         'platform': 'platform_accession_codes',
         'organism': 'organism_names',
+        'num_processed_samples': {
+            'field': 'num_processed_samples',
+            'lookups': [
+                LOOKUP_FILTER_RANGE,
+                LOOKUP_QUERY_IN,
+                LOOKUP_QUERY_GT
+            ],
+        },
         'num_downloadable_samples': {
             'field': 'num_downloadable_samples',
             'lookups': [
