639657271c00d006fe5ab2174424076dc40a46e8
Kurt Wheeler <kurt.wheeler91@gmail.com>
2019-07-05 16:47:31 -0400

    Refactors run_tximport command to make it easier to test and adds tests for it.

--- foreman/data_refinery_foreman/foreman/management/commands/run_tximport.py
+++ foreman/data_refinery_foreman/foreman/management/commands/run_tximport.py
@@ -35,46 +35,55 @@ from data_refinery_common.utils import get_env_variable, get_active_volumes
 
 logger = get_and_configure_logger(__name__)
 
+def run_tximport():
+    """Creates a tximport job for all eligible experiments."""
+    eligible_experiments = Experiment.objects.extra(
+        where=['cardinality(organism_names) = 1']
+    ).filter(
+        technology='RNA-SEQ',
+        num_processed_samples=0
+    ).prefetch_related('samples__results')
+
+    # Next is to figure out how many samples were processed for
+    # each experiment. Should be able to reuse code from salmon
+    # cause it does this stuff.
+    tximport_pipeline = ProcessorPipeline.TXIMPORT
+    for experiment in eligible_experiments:
+        quant_results = get_quant_results_for_experiment(experiment)
+
+        if should_run_tximport(experiment, len(quant_results), True):
+            processor_job = ProcessorJob()
+            processor_job.pipeline_applied = tximport_pipeline.value
+            processor_job.ram_amount = 8192
+            # This job doesn't need to run on a specific volume
+            # but it uses the same Nomad job as Salmon jobs which
+            # do require the volume index.
+            processor_job.volume_index = random.choice(list(get_active_volumes()))
+            processor_job.save()
+
+            assoc = ProcessorJobOriginalFileAssociation()
+            # Any original file linked to any sample of the
+            # experiment will work. Tximport is somewhat special
+            # in that it doesn't actuallhy use original files so
+            # this is just used to point to the experiment.
+            samples = experiment.samples.all()
+            assoc.original_file = experiment.samples.all()[0].original_files.all()[0]
+            assoc.processor_job = processor_job
+            assoc.save()
+
+            try:
+                send_job(tximport_pipeline, processor_job)
+            except:
+                # If we cannot queue the job now the Foreman will do
+                # it later.
+                pass
+
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+        """This command just calls run_tximport.
 
-        eligible_experiments = Experiment.objects.extra(
-            select={'num_organisms':'cardinality(organism_names)'}
-        ).filter(
-            'num_organisms'=1,
-            technology='RNA-SEQ',
-            num_processed_samples=0
-        ).prefetch_related('samples__results__computed_files')
-
-        # Next is to figure out how many samples were processed for
-        # each experiment. Should be able to reuse code from salmon
-        # cause it does this stuff.
-        tximport_pipeline = ProcessorPipeline.TXIMPORT
-        for experiment in eligible_experiments:
-            quant_results = get_quant_results_for_experiment(experiment)
-            if should_run_tximport(experiment, len(quant_results), True):
-                processor_job = ProcessorJob()
-                processor_job.pipeline_applied = tximport_pipeline.value
-                processor_job.ram_amount = 8192
-                # This job doesn't need to run on a specific volume
-                # but it uses the same Nomad job as Salmon jobs which
-                # do require the volume index.
-                processor_job.volume_index = random.choice(get_active_volumes())
-                processor_job.save()
-
-                assoc = ProcessorJobOriginalFileAssociation()
-                # Any original file linked to any sample of the
-                # experiment will work. Tximport is somewhat special
-                # in that it doesn't actuallhy use original files so
-                # this is just used to point to the experiment.
-                assoc.original_file = experiment.samples.all()[0].original_files.all()[0]
-                assoc.processor_job = processor_job
-                assoc.save()
-
-                try:
-                    send_job(tximport_pipeline, processor_job)
-                except:
-                    # If we cannot queue the job now the Foreman will do
-                    # it later.
-                    pass
+        The functionality has been broken out into a separate function
+        to make testing easy.
+        """
+        run_tximport()
