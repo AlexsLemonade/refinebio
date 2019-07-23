79f2b0edb5a92c4686bed65a493cc5089aa589f7
Ariel <arielsvn@gmail.com>
2019-07-23 14:58:58 -0400

    Add migration to update sample platform names

---
+++ common/data_refinery_common/migrations/0023_fix_platform_names_spaces.py
@@ -0,0 +1,22 @@
+
+from django.db import migrations
+from data_refinery_common.utils import get_supported_rnaseq_platforms
+from django.core.paginator import Paginator
+
+def make_sample_platform_names_readable(apps, schema_editor):
+    Sample = apps.get_model('data_refinery_common', 'Sample')
+
+    for platform_name in get_supported_rnaseq_platforms():
+        munged_platform = platform_name.replace(' ', '')
+        Sample.objects.all().filter(platform_name=munged_platform).update(platform_name=platform_name)
+        print("Updating platform name from \'%s\' to \'%s\' page" % (munged_platform, platform_name))
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('data_refinery_common', '0022_auto_20190607_1505'),
+    ]
+
+    operations = [
+        migrations.RunPython(make_sample_platform_names_readable)
+    ]
