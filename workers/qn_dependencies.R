e8cbafeceef84f772ad475b0627486598f4e0a4e
Will Vauclain <willvauclain.dev@gmail.com>
2019-08-01 10:25:54 -0400

    Freeze all R dependencies using cranlock

--- workers/qn_dependencies.R
+++ workers/qn_dependencies.R
@@ -1,11 +1,6 @@
 options(warn=2)
-options(repos=structure(c(CRAN="https://cran.microsoft.com/snapshot/2019-07-03")))
+options(repos=structure(c(CRAN="https://cran.revolutionanalytics.com")))
 options(Ncpus=parallel::detectCores())
-
-devtools::install_version('doParallel', version='1.0.11')
-devtools::install_version('data.table', version='1.11.0')
-devtools::install_version('optparse', version='1.4.4')
-devtools::install_version('lazyeval', version='0.2.1')
 
 # devtools::install_url() requires BiocInstaller
 install.packages('https://bioconductor.org/packages/3.6/bioc/src/contrib/BiocInstaller_1.28.0.tar.gz')
