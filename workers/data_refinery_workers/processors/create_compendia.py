6c5612d5f6915d1974788a945c26f2a536ea5497
Kurt Wheeler <kurt.wheeler91@gmail.com>
2019-08-01 13:26:51 -0400

    README_COMPENDIA->README_COMPENDIUM....

--- workers/data_refinery_workers/processors/create_compendia.py
+++ workers/data_refinery_workers/processors/create_compendia.py
@@ -280,7 +280,7 @@ def _create_result_objects(job_context: Dict) -> Dict:
     # Create the resulting archive
     final_zip_base = "/home/user/data_store/smashed/" + str(job_context["dataset"].pk) + "_compendia"
     # Copy LICENSE.txt and README.md files
-    shutil.copy("/home/user/data_store/README_COMPENDIA.md", final_zip_base + "/README.md")
+    shutil.copy("/home/user/data_store/README_COMPENDIUM.md", final_zip_base + "/README.md")
     shutil.copy("/home/user/data_store/LICENSE_DATASET.txt", final_zip_base + "/LICENSE.TXT")
     archive_path = shutil.make_archive(final_zip_base, 'zip', job_context["output_dir"])
 
