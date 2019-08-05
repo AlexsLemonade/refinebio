e4531f7dbb3e91957aea39857c854424b368437c
Will Vauclain <willvauclain.dev@gmail.com>
2019-08-05 08:50:56 -0400

    Remove terraform completely from circleci logs

--- .circleci/remote_deploy.sh
+++ .circleci/remote_deploy.sh
@@ -101,9 +101,9 @@ echo "Finished building new images, running run_terraform.sh."
 
 run_on_deploy_box "sudo touch /var/log/deploy_$CIRCLE_TAG.log"
 run_on_deploy_box "sudo chown ubuntu:ubuntu /var/log/deploy_$CIRCLE_TAG.log"
-run_on_deploy_box "source env_vars && echo -e '######\nStarting new deploy for $CIRCLE_TAG\n######' 2>&1 | tee -a /var/log/deploy_$CIRCLE_TAG.log"
+run_on_deploy_box "source env_vars && echo -e '######\nStarting new deploy for $CIRCLE_TAG\n######' >> /var/log/deploy_$CIRCLE_TAG.log 2>&1"
 run_on_deploy_box "source env_vars && ./.circleci/run_terraform.sh >> /var/log/deploy_$CIRCLE_TAG.log 2>&1"
-run_on_deploy_box "source env_vars && echo -e '######\nDeploying $CIRCLE_TAG finished!\n######' 2>&1 | tee -a /var/log/deploy_$CIRCLE_TAG.log"
+run_on_deploy_box "source env_vars && echo -e '######\nDeploying $CIRCLE_TAG finished!\n######' >> /var/log/deploy_$CIRCLE_TAG.log 2>&1"
 
 # Don't leave secrets lying around.
 ## Clean out any files we've created or moved so git-crypt will relock the repo.
