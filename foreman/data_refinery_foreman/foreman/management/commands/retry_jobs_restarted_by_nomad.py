e9a7e04d50d1c9aac6c476e18310e8dd6a8146b6
Ariel <arielsvn@gmail.com>
2019-08-28 11:20:34 -0500

    address pr comments

--- foreman/data_refinery_foreman/foreman/management/commands/retry_jobs_restarted_by_nomad.py
+++ foreman/data_refinery_foreman/foreman/management/commands/retry_jobs_restarted_by_nomad.py
@@ -16,33 +16,29 @@ from data_refinery_common.job_management import create_downloader_job
 logger = get_and_configure_logger(__name__)
 
 def retry_jobs_restarted_by_nomad():
-    """Creates a tximport job for all eligible experiments."""
     latest_processor_job_for_sample = ProcessorJob.objects\
             .filter(original_files__samples=OuterRef('id'))\
             .order_by('-start_time')
 
-    eligible_sample_ids = Sample.objects\
-      .annotate(
-        failure_reason=Subquery(latest_processor_job_for_sample.values('failure_reason')[:1])
-      )\
-      .filter(
-        failure_reason__startswith='ProcessorJob has already completed'
-      )\
-      .values_list('id', flat=True)
-
-    eligible_samples = Sample.objects.filter(id__in=eligible_sample_ids)\
-                             .prefetch_related('original_files')
+    eligible_samples = Sample.objects\
+        .annotate(
+            failure_reason=Subquery(latest_processor_job_for_sample.values('failure_reason')[:1])
+        )\
+        .filter(
+            failure_reason__startswith='ProcessorJob has already completed'
+        )\
+        .prefetch_related('original_files')
 
     total_samples_queued = 0
     for sample in eligible_samples:
-      original_files = list(sample.original_files.all())
-      if not len(original_files): continue
+        original_files = list(sample.original_files.all())
+        if not len(original_files): continue
 
-      create_downloader_job(original_files)
-      total_samples_queued += 1
+        if create_downloader_job(original_files):
+            total_samples_queued += 1
 
     logger.info("Re-queued %d samples that had failed .", total_samples_queued)
 
 class Command(BaseCommand):
-  def handle(self, *args, **options):
-    retry_jobs_restarted_by_nomad()
+    def handle(self, *args, **options):
+        retry_jobs_restarted_by_nomad()
