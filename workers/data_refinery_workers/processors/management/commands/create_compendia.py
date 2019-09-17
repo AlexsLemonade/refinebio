77c9926b60c5a5e182fc7a6a135ff33fa3dc608d
Kurt Wheeler <kurt.wheeler91@gmail.com>
2019-09-17 10:34:13 -0400

    Make create_compendia dispatch jobs if requested to create more than one.

--- workers/data_refinery_workers/processors/management/commands/create_compendia.py
+++ workers/data_refinery_workers/processors/management/commands/create_compendia.py
@@ -13,12 +13,40 @@ from data_refinery_common.models import (
     Dataset,
     ProcessorJobDatasetAssociation,
     ExperimentOrganismAssociation,
-    OrganismIndex,
     ExperimentSampleAssociation
 )
 from data_refinery_workers.processors import create_compendia
 
 logger = get_and_configure_logger(__name__)
+
+
+def create_job_for_organism(organism=Organism):
+    """Returns a compendia job for the provided organism.
+
+    Fetch all of the experiments and compile large but normally formated Dataset.
+    """
+    data = {}
+    experiments = Experiment.objects.filter(id__in=(ExperimentOrganismAssociation.objects.filter(organism=organism)).values('experiment'))
+    for experiment in experiments:
+        data[experiment.accession_code] = list(experiment.samples.filter(organism=organism).values_list('accession_code', flat=True))
+
+    job = ProcessorJob()
+    job.pipeline_applied = "COMPENDIA"
+    job.save()
+
+    dset = Dataset()
+    dset.data = data
+    dset.scale_by = 'NONE'
+    dset.aggregate_by = 'SPECIES'
+    dset.quantile_normalize = False
+    dset.save()
+
+    pjda = ProcessorJobDatasetAssociation()
+    pjda.processor_job = job
+    pjda.dataset = dset
+    pjda.save()
+
+    return job
 
 
 class Command(BaseCommand):
@@ -30,40 +58,22 @@ class Command(BaseCommand):
             help=("Name of organism"))
 
     def handle(self, *args, **options):
-        """ For every (or a supplied) organism, fetch all of the experiments and compile large but normally formated Dataset.
+        """Create a compendium for one or more organisms.
 
-        Send all of them to the Smasher. Smash them. Retrieve manually as desired.
+        If --organism is supplied will immediately create a compedium
+        for it. If not a new job will be dispatched for each organism
+        with enough microarray samples.
         """
-
-        dataset_ids = []
-
         if options["organism"] is None:
             all_organisms = Organism.objects.all()
+
+            for organism in all_organisms:
+                job = create_job_for_organism(organism)
+                logger.info("Sending CREATE_COMPENDIA for Organism", job_id=str(job.pk), organism=str(organism))
+                send_job(ProcessorPipeline.CREATE_COMPENDIA, job)
         else:
-            all_organisms = [Organism.get_object_for_name(options["organism"].upper())]
-
-        for organism in all_organisms:
-            data = {}
-            experiments = Experiment.objects.filter(id__in=(ExperimentOrganismAssociation.objects.filter(organism=organism)).values('experiment'))
-            for experiment in experiments:
-                data[experiment.accession_code] = list(experiment.samples.filter(organism=organism).values_list('accession_code', flat=True))
-
-            job = ProcessorJob()
-            job.pipeline_applied = "COMPENDIA"
-            job.save()
-
-            dset = Dataset()
-            dset.data = data
-            dset.scale_by = 'NONE'
-            dset.aggregate_by = 'SPECIES'
-            dset.quantile_normalize = False
-            dset.save()
-
-            pjda = ProcessorJobDatasetAssociation()
-            pjda.processor_job = job
-            pjda.dataset = dset
-            pjda.save()
-
-            final_context = create_compendia.create_compendia(job.id)
+            organism = Organism.get_object_for_name(options["organism"].upper())
+            job = create_job_for_organism(organism)
+            create_compendia.create_compendia(job.id)
 
         sys.exit(0)
