1bf47397de56d465e932f91096b4083bc25c79cc
David Mejia <davidsmejia@gmail.com>
2019-09-24 15:25:15 -0400

    evaluate map object

--- workers/data_refinery_workers/processors/smasher.py
+++ workers/data_refinery_workers/processors/smasher.py
@@ -639,7 +639,7 @@ def _smash(job_context: Dict, how="inner") -> Dict:
             #cpus = max(1, psutil.cpu_count()/2)
             #with multiprocessing.Pool(int(cpus)) as pool:
             #    processed_frames = pool.map(
-            processed_frames = map(
+            mapped_frames = map(
                 process_frame,
                 [(
                     job_context,
@@ -648,7 +648,7 @@ def _smash(job_context: Dict, how="inner") -> Dict:
                     i
                 ) for i, (computed_file, sample) in enumerate(input_files)
             ])
-           
+            processed_frames = list(mapped_frames)
             all_frames = [f['data'] for f in processed_frames if f['data'] is not None]
             num_samples = num_samples + len([x for x in all_frames])
             unsmashable_files = unsmashable_files + [f['unsmashable_file'] for f in processed_frames if f['unsmashable']]
