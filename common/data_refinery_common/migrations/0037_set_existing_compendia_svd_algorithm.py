493f9d97e736479b60576b9abd32427096314b83
David Mejia <davidsmejia@gmail.com>
2019-09-24 18:00:46 -0400

    fix help_text and  change ARPACK to caps

--- common/data_refinery_common/migrations/0037_set_existing_compendia_svd_algorithm.py
+++ common/data_refinery_common/migrations/0037_set_existing_compendia_svd_algorithm.py
@@ -1,15 +1,15 @@
 from django.db import migrations, models
 
 def set_existing_compendia_svd_algorithm(apps, schema_editor):
-    """ Fixes affymetrix samples that have their manufacturer set to "AFFYMETRTIX" or "NEXTSEQ" """
+    """ Fixes existing compendia svd algorithm - defaulted NONE corrects to ARPACK """ 
     ComputedFile = apps.get_model('data_refinery_common', 'ComputedFile')
-    ComputedFile.objects.all().filter(is_compendia=True).update(svd_algorithm="arpack")
+    ComputedFile.objects.all().filter(is_compendia=True).update(svd_algorithm="ARPACK")
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('data_refinery_common', '0036_auto_20190919_2306'),
+        ('data_refinery_common', '0036_auto_20190924_2153'),
     ]
 
     operations = [
