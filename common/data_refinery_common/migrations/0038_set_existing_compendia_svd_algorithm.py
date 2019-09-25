c7b325cfcd85ee735a94106249324ac8fbf5fcf8
David Mejia <davidsmejia@gmail.com>
2019-09-24 20:51:23 -0400

    filter queryset before looping over page

--- common/data_refinery_common/migrations/0038_set_existing_compendia_svd_algorithm.py
+++ common/data_refinery_common/migrations/0038_set_existing_compendia_svd_algorithm.py
@@ -5,11 +5,10 @@ def set_existing_compendia_svd_algorithm(apps, schema_editor):
     """ Fixes existing compendia svd algorithm - defaulted NONE corrects to ARPACK """ 
     ComputedFile = apps.get_model('data_refinery_common', 'ComputedFile')
 
-    paginator = Paginator(ComputedFile.objects.all().order_by('id'), 1000)
+    paginator = Paginator(ComputedFile.objects.all().order_by('id').filter(is_compendia=True), 1000)
     for page_idx in range(1, paginator.num_pages):
         for computedFile in paginator.page(page_idx).object_list:
-            if computedFile.is_compendia:
-                computedFile.update(svd_algorithm="ARPACK")
+            computedFile.update(svd_algorithm="ARPACK")
 
 
 class Migration(migrations.Migration):
