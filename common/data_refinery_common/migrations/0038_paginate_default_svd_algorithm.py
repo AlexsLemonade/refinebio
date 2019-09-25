733062ecb13134dae1ccfa9ca12a5faca407da95
Kurt Wheeler <kurt.wheeler91@gmail.com>
2019-09-25 11:31:35 -0400

    Update each page of the migration.

--- common/data_refinery_common/migrations/0038_paginate_default_svd_algorithm.py
+++ common/data_refinery_common/migrations/0038_paginate_default_svd_algorithm.py
@@ -1,22 +1,26 @@
 from django.db import migrations, models
 from django.core.paginator import Paginator
 
-def set_computed_file_svd_algorithm(apps, schema_editor):
+def set_computed_file_svd_algorithm_arpack(apps, schema_editor):
     """ Set svd_algorithm for computed files - ARPACK when is_compendia"""
     ComputedFile = apps.get_model('data_refinery_common', 'ComputedFile')
-    paginator = Paginator(ComputedFile.objects.all().order_by('id'), 1000)
+    paginator = Paginator(ComputedFile.objects.filter(is_compendia=True).order_by('id'), 1000)
     for page_idx in range(1, paginator.num_pages):
-        for computed_file in paginator.page(page_idx).object_list:
-            svd_algorithm = "ARPACK" if computed_file.is_compendia else "NONE"
-            computed_file.update(svd_algorithm=svd_algorithm)
+        paginator.page(page_idx).object_list.update(svd_algorithm="ARPACK")
+
+def set_computed_file_svd_algorithm_none(apps, schema_editor):
+    """ Set svd_algorithm for computed files - None when not is_compendia"""
+    ComputedFile = apps.get_model('data_refinery_common', 'ComputedFile')
+    paginator = Paginator(ComputedFile.objects.filter(is_compendia=False).order_by('id'), 1000)
+    for page_idx in range(1, paginator.num_pages):
+        paginator.page(page_idx).object_list.update(svd_algorithm="None")
 
 def set_dataset_svd_algorithm_none(apps, schema_editor):
-    """ Apply default svd_algorithm via pagination to dataset """    
+    """ Apply default svd_algorithm via pagination to dataset """
     Dataset = apps.get_model('data_refinery_common', 'Dataset')
     paginator = Paginator(Dataset.objects.all().order_by('id'), 1000)
     for page_idx in range(1, paginator.num_pages):
-        for dataset in paginator.page(page_idx).object_list:
-            dataset.update(svd_algorithm="NONE")
+        paginator.page(page_idx).object_list.update(svd_algorithm="NONE")
 
 class Migration(migrations.Migration):
 
@@ -25,6 +29,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(set_computed_file_svd_algorithm),
+        migrations.RunPython(set_computed_file_svd_algorithm_arpack),
+        migrations.RunPython(set_computed_file_svd_algorithm_none),
         migrations.RunPython(set_dataset_svd_algorithm_none),
     ]
