12b500bb0af91febf48952f10294fe37c3490619
David Mejia <davidsmejia@gmail.com>
2019-11-01 11:36:19 -0400

    migration to populate compendia results

---
+++ common/data_refinery_common/migrations/0043_populate_compendia_results.py
@@ -0,0 +1,33 @@
+from django.db import migrations, models
+
+def populate_compendia_results(apps, schema):
+    """ Populate compendia_results with computed files """
+
+    ComputedFile = apps.get_model('data_refinery_common', 'ComputedFile')
+    CompendiaResult = apps.get_model('data_refinery_common', 'CompendiaResult')
+
+    # get all existing compendia computed files
+    compendia_computed_files = ComputedFile.objects.filter(is_compendia=True)
+
+    # create as compendia result from each of the results 
+    for computed_file in compendia_computed_files:
+        compendia_result = CompendiaResult(quant_sf_only=computed_file.quant_sf_only,
+                                           compendia_version=computed_file.compendia_version,
+                                           svd_algorithm=computed_file.svd_algorithm)
+
+        # relationships
+        compendia_result.result = computed_file.result
+        compendia_result.primary_organism = computed_file.compendia_organism
+        compendia_result.organisms.add(computed_file.compendia_organism)
+
+        compendia_result.save()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('data_refinery_common', '0042_auto_20191031_1953'),
+    ]
+
+    operations = [
+        migrations.RunPython(populate_compendia_results)
+    ]
