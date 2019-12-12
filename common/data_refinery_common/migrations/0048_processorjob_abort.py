9a6571646c6361a1308346d64244da22076149c7
Ariel Rodriguez Romero <arielsvn@gmail.com>
2019-12-12 15:46:48 -0500

    Revert "Merge pull request #1992 from AlexsLemonade/arielsvn/hotfix-about-column-migration"
    This reverts commit 9d63dec90dd12dae98dc43dc3bfc3de63242e52a.

--- common/data_refinery_common/migrations/0048_processorjob_abort.py
+++ common/data_refinery_common/migrations/0048_processorjob_abort.py
@@ -30,16 +30,31 @@ class Migration(migrations.Migration):
     ]
 
     # we need to add a new field `abort` with default value `False`
-    # The processor jobs table is big, that's why we add the field with no default
-    # and then set the default value
-    # the next migration alters the field to have a default value
+    # The processor jobs table is big, so it the migration might time out
+    # that's why we add the default value in batches
     operations = [
-        # 1. add fields with None as default value
+        # 1. remove indexes
+        migrations.RemoveIndex(
+            model_name='processorjob',
+            name='processor_jobs_created_at',
+        ),
+        # 2. add fields with None as default value
         migrations.AddField(
             model_name='processorjob',
             name='abort',
             field=models.BooleanField(null=True),
         ),
-        # 2. set default value in batches
+        # 3. set default value in batches
         migrations.RunPython(set_default_abort_value),
+        # 4. add default value for field
+        migrations.AlterField(
+            model_name='processorjob',
+            name='abort',
+            field=models.BooleanField(default=False, null=False),
+        ),
+        # 5. re-add indexes
+        migrations.AddIndex(
+            model_name='processorjob',
+            index=models.Index(fields=['created_at'], name='processor_jobs_created_at'),
+        ),
     ]
