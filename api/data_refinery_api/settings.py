a0d5e3abc2005b4369fc37614b988497aadadaa1
David Mejia <davidsmejia@gmail.com>
2019-12-13 14:02:29 -0500

    remove whitespace

--- api/data_refinery_api/settings.py
+++ api/data_refinery_api/settings.py
@@ -67,7 +67,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-
     'data_refinery_api.middleware.RevisionMiddleware',
     'data_refinery_api.middleware.SentryCatchBadRequestMiddleware',
     'django.middleware.security.SecurityMiddleware',
