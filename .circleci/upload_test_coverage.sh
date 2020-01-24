40b44fec57334052b8b8c62944b9312f4060208a
Ariel <arielsvn@gmail.com>
2020-01-24 16:57:39 -0500

    remove flags parameter

--- .circleci/upload_test_coverage.sh
+++ .circleci/upload_test_coverage.sh
@@ -16,4 +16,4 @@ output="test_volume/${project}_coverage.xml"
 # folder name to each file path in coverage.xml
 sed "s/filename=\"/filename=\"$project\//g" test_volume/coverage.xml > $output
 
-curl -s https://codecov.io/bash | bash -s -- -f "$output" -F unittests
+curl -s https://codecov.io/bash | bash -s -- -f "$output"
