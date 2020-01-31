a18546dfbf5c97933adf3e05f881e796a4c35611
Ariel <arielsvn@gmail.com>
2020-01-31 11:32:57 -0500

    update comment

--- .circleci/upload_test_coverage.sh
+++ .circleci/upload_test_coverage.sh
@@ -27,9 +27,9 @@ fi
 
 output_file="${test_volume}/${project}_coverage.xml"
 
-# In the test coverage report, all file paths are absolute to each project
-# folder. We need to be relative to refinebio/ that's why we append the project
-# folder name to each file path in coverage.xml
+# In the test coverage report, all file paths are relative to each project
+# folder. We need to be relative to the repo's root directory. That's why we
+# append the project folder name to each file path in coverage.xml
 sed "s/filename=\"/filename=\"$project\//g" $coverage_file > $output_file
 
 curl -s https://codecov.io/bash | bash -s -- -f "$output_file" -Z -F $project
