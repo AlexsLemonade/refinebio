330ed009756002c57b762cf6b29332d8126322bc
David Mejia <davidsmejia@gmail.com>
2020-03-13 11:34:11 -0400

    change logs to cloudwatch logs

--- infrastructure/disk.tf
+++ infrastructure/disk.tf
@@ -114,13 +114,13 @@ resource "aws_s3_bucket" "data_refinery_compendia_bucket" {
   }
 }
 
-resource "aws_s3_bucket" "data_refinery_logs_bucket" {
+resource "aws_s3_bucket" "data_refinery_cloudtrail_logs_bucket" {
   bucket = "data-refinery-s3-logs-${var.user}-${var.stage}"
   acl = "private"
   force_destroy = "${var.static_bucket_prefix == "dev" ? true : false}"
 
   tags {
-    Name        = "data-refinery-s3-logs-${var.user}-${var.stage}"
+    Name        = "data-refinery-s3-cloudtrail-logs-${var.user}-${var.stage}"
     Environment = "${var.stage}"
   }
 }
