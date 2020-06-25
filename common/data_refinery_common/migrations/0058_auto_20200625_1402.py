a3ac5dbe0289cba41f03c3d91f76e69c91315104
Will Vauclain <willvauclain.dev@gmail.com>
2020-06-25 11:04:46 -0400

    Ran black on migration

--- common/data_refinery_common/migrations/0058_auto_20200625_1402.py
+++ common/data_refinery_common/migrations/0058_auto_20200625_1402.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('data_refinery_common', '0057_fix_platform_accessions_spaces'),
+        ("data_refinery_common", "0057_fix_platform_accessions_spaces"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='dataset',
-            name='email_address',
+            model_name="dataset",
+            name="email_address",
             field=models.EmailField(blank=True, max_length=255, null=True),
         ),
     ]
