86f0ea3d9c6138f4f53a382db646c95bbe642276
Will Vauclain <willvauclain.dev@gmail.com>
2020-07-06 12:04:49 -0400

    Add missing import in URLs file

--- api/data_refinery_api/urls.py
+++ api/data_refinery_api/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls import url
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import include, path
 from django.views.generic import RedirectView
 from rest_framework import permissions
