a0f1747cb71c5b16704eecc0c89dffd63f44eeb8
Kurt Wheeler <kurt.wheeler91@gmail.com>
2020-08-20 12:17:56 -0400

    Make shellcheck happy.

--- .github/scripts/git_decrypt.sh
+++ .github/scripts/git_decrypt.sh
@@ -2,7 +2,7 @@
 
 # Unlock encrypted files
 # Temporary attempt to debug this.
-cd $GITHUB_WORKSPACE/.github/ || exit
+cd "$GITHUB_WORKSPACE"/.github/ || exit
 git clean -f
 openssl aes-256-cbc -md md5 -d -in git_crypt.key.enc -out git_crypt.key -k "$OPENSSL_KEY"
 git-crypt unlock git_crypt.key
