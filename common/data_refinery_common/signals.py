75bf9851b89263a33ab86caafbf718c0a2b126ae
BEW111 <bewilliams111@gmail.com>
2020-08-25 14:22:23 -0400

    removed original file related changes

--- common/data_refinery_common/signals.py
+++ common/data_refinery_common/signals.py
@@ -7,7 +7,6 @@ from data_refinery_common.models import ComputedFile, OriginalFile
 logger = get_and_configure_logger(__name__)
 
 
-@receiver(pre_delete, sender=OriginalFile)
 @receiver(pre_delete, sender=ComputedFile)
 def remove_file_from_s3(sender, instance, **kwargs):
     """ When the local model is about to be deleted, try to delete the s3 file
