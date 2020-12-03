edf40e8321493ae98780f9bd29ef84fb55c5d626
Kurt Wheeler <kurt.wheeler91@gmail.com>
2020-12-03 12:03:39 -0500

    This uses a specific bioconductor URL for all its deps, hopefully these don't go anywhere...

--- workers/install_downloader_R_only.R
+++ workers/install_downloader_R_only.R
@@ -6,9 +6,10 @@ options(Ncpus=parallel::detectCores())
 
 # Bioconductor packages, installed by devtools::install_url()
 
-# Helper function that installs a list of packages based on input URL
-install_with_url <- function(main_url, packages) {
-  pkg_ids <- devtools::install_url(paste0(main_url, packages))
+
+# Helper function that installs a list of packages using the input URLs
+install_with_url <- function(urls) {
+  pkg_ids <- devtools::install_url(urls)
   if(any(is.na(pkg_ids))) {
     pkg_fails <- paste(packages[is.na(pkg_ids)], collapse = "; ")
     stop(paste("Failed to install package(s):", pkg_fails ))
@@ -16,9 +17,8 @@ install_with_url <- function(main_url, packages) {
   return(pkg_ids)
 }
 
-bioc_url <- 'https://bioconductor.org/packages/release/bioc/src/contrib/'
 bioc_pkgs <- c(
-  'affyio_1.60.0.tar.gz',
-  'zlibbioc_1.36.0.tar.gz'
+  'https://bioconductor.org/packages/3.12/bioc/src/contrib/affyio_1.60.0.tar.gz',
+  'https://bioconductor.org/packages/3.12/bioc/src/contrib/zlibbioc_1.36.0.tar.gz'
 )
-install_with_url(bioc_url, bioc_pkgs)
+install_with_url(bioc_pkgs)
