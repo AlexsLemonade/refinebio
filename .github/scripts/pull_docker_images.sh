ea7e29cac0756883ec973cc6e4411cf516abca41
Kurt Wheeler <kurt.wheeler91@gmail.com>
2020-12-16 14:15:07 -0500

    Remove unintentional additional line.

--- .github/scripts/pull_docker_images.sh
+++ .github/scripts/pull_docker_images.sh
@@ -4,7 +4,6 @@ set -e
 
 REPO=$(echo "docker.pkg.github.com/$GITHUB_REPOSITORY" | tr '[:upper:]' '[:lower:]')
 if [ -z "$IMAGES" ]; then
-
     echo "Error: must put images to pull in \$IMAGES" >&2
     exit 1
 fi
