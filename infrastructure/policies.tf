3658c0cefb6ff221b052ecba78c50f035e6e59d8
Kurt Wheeler <kurt.wheeler91@gmail.com>
2021-03-10 12:29:11 -0500

    I forgot this in my last PR.

---
+++ infrastructure/policies.tf
@@ -0,0 +1,156 @@
+locals {
+  s3_access_policy = <<EOF
+{
+   "Version":"2012-10-17",
+   "Statement":[
+      {
+         "Effect":"Allow",
+         "Action":[
+            "s3:ListAllMyBuckets"
+         ],
+         "Resource":"arn:aws:s3:::*"
+      },
+      {
+         "Effect":"Allow",
+         "Action":[
+            "s3:ListBucket",
+            "s3:GetBucketLocation"
+         ],
+         "Resource":"arn:aws:s3:::data-refinery"
+      },
+      {
+         "Effect":"Allow",
+         "Action":[
+            "s3:PutObject",
+            "s3:PutObjectAcl",
+            "s3:GetObject",
+            "s3:DeleteObject"
+         ],
+          "Resource": [
+            "arn:aws:s3:::${aws_s3_bucket.data_refinery_bucket.bucket}/*",
+            "arn:aws:s3:::${aws_s3_bucket.data_refinery_results_bucket.bucket}/*",
+            "arn:aws:s3:::${aws_s3_bucket.data_refinery_transcriptome_index_bucket.bucket}/*",
+            "arn:aws:s3:::${aws_s3_bucket.data_refinery_qn_target_bucket.bucket}/*",
+            "arn:aws:s3:::${aws_s3_bucket.data_refinery_compendia_bucket.bucket}/*"
+          ]
+      }
+   ]
+}
+EOF
+
+  # Policy text found at:
+  # http://docs.aws.amazon.com/AmazonCloudWatch/latest/logs/iam-identity-based-access-control-cwl.html
+  cloudwatch_logs_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents",
+                "logs:DescribeLogStreams",
+                "cloudwatch:GetMetricStatistics",
+                "cloudwatch:ListMetrics",
+                "cloudwatch:PutMetricAlarm",
+                "cloudwatch:PutMetricData",
+                "cloudwatch:SetAlarmState"
+            ],
+            "Resource": [
+              "arn:aws:logs:${var.region}:${data.aws_caller_identity.current.account_id}:log-group:${aws_cloudwatch_log_group.data_refinery_log_group.name}:*"
+            ]
+        }
+    ]
+}
+EOF
+
+  cloudtrail_access_policy = <<EOF
+{
+  "Statement": [
+    {
+      "Action": "s3:GetBucketAcl",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "cloudtrail.amazonaws.com"
+      },
+      "Resource": "${aws_s3_bucket.data_refinery_cloudtrail_logs_bucket.arn}",
+      "Sid": "AWSCloudTrailAclCheck20150319"
+    },
+    {
+      "Action": "s3:PutObject",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "cloudtrail.amazonaws.com"
+      },
+      "Resource": "${aws_s3_bucket.data_refinery_cloudtrail_logs_bucket.arn}/*",
+      "Sid": "AWSCloudTrailWrite20150319"
+    }
+  ],
+  "Version": "2012-10-17"
+}
+EOF
+
+  batch_read_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "batch:ListJobs"
+            ],
+            "Resource": [
+              "*"
+            ]
+        }
+    ]
+}
+EOF
+
+  batch_write_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "batch:SubmitJob"
+            ],
+            "Resource": [
+              "arn:aws:batch:${var.region}:${data.aws_caller_identity.current.account_id}:*"
+            ]
+        }
+    ]
+}
+EOF
+
+  ecs_instance_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:DescribeTags",
+                "ecs:CreateCluster",
+                "ecs:DeregisterContainerInstance",
+                "ecs:DiscoverPollEndpoint",
+                "ecs:Poll",
+                "ecs:RegisterContainerInstance",
+                "ecs:StartTelemetrySession",
+                "ecs:UpdateContainerInstancesState",
+                "ecs:Submit*",
+                "ecr:GetAuthorizationToken",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
