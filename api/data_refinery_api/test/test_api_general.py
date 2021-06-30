6bbb5125676a80d3a99019e92fbbbb76eedef8af
Kurt Wheeler <kurt.wheeler91@gmail.com>
2021-06-30 12:51:19 -0400

    Try using a lighterweight endpoint for testing 429s....

--- api/data_refinery_api/test/test_api_general.py
+++ api/data_refinery_api/test/test_api_general.py
@@ -343,9 +343,7 @@ class APITestCases(APITestCase):
         # Tenth call since reset_cache() should be throttled, three have happened.
         # Make more than necessary to ensure we get the throttle.
         for i in range(15):
-            response = self.client.get(
-                reverse("transcriptome_indices", kwargs={"version": API_VERSION})
-            )
+            response = self.client.get(reverse("survey_jobs", kwargs={"version": API_VERSION}))
         self.assertEqual(response.status_code, status.HTTP_429_TOO_MANY_REQUESTS)
 
     def test_experiment_multiple_accessions(self):
