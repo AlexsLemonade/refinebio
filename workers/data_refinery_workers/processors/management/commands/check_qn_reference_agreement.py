ee554230ebc1cad97fab84d927c8313b0273136a
Will Vauclain <willvauclain.dev@gmail.com>
2021-08-10 10:58:22 -0400

    QN reference cron job test fixes from my devstack

--- workers/data_refinery_workers/processors/management/commands/check_qn_reference_agreement.py
+++ workers/data_refinery_workers/processors/management/commands/check_qn_reference_agreement.py
@@ -20,11 +20,11 @@ KS_TEST_THRESHOLD = 0.05
 
 
 def signal_failure(bad_organisms):
-    if not settings.RUNNING_IN_CLOUD:
-        logger.error("Cannot message Slack when we are not running in the cloud")
-        return
-    elif not settings.ENGAGEMENTBOT_WEBHOOK:
-        logger.error("Cannot message slack because we don't have a valid webhook")
+    if not settings.RUNNING_IN_CLOUD or not settings.ENGAGEMENTBOT_WEBHOOK:
+        logger.info(
+            "Some newly-generated QN references were not similar-enough to our references currently in use.",
+            failing_organisms=", ".join(o.name for o in bad_organisms),
+        )
         return
 
     requests.post(
@@ -60,33 +60,29 @@ def check_qn_reference_agreement():
             platform=create_qn_target.get_biggest_platform(organism),
             create_results=False,
         )
-        new_target = pd.read_csv(
-            final_context["target_file"],
-            index=False,
-            index_col=0,
-            header=False,
-            sep="\t",
-            encoding="utf-8",
-        )
+
+        if final_context.get("success") is False:
+            raise Exception(f"create_qn_target failed: {final_context['job'].failure_reason}")
+
+        new_target = final_context["sum_frame"]["sum"]
 
         existing_target_computedfile = organism.qn_target.computedfile_set.latest()
         existing_target_filename = existing_target_computedfile.sync_from_s3()
         existing_target = pd.read_csv(
-            existing_target_filename,
-            index=False,
-            index_col=0,
-            header=False,
-            sep="\t",
-            encoding="utf-8",
-        )
+            existing_target_filename, header=None, sep="\t", encoding="utf-8"
+        )[0]
         # We don't want this file hanging around on the foreman for too long
         existing_target_computedfile.delete_local_file()
 
         _, pvalue = scipy.stats.ks_2samp(new_target, existing_target)
 
-        # TODO: decide on a good threshold
         if pvalue < 0.05:
+            logger.error(
+                "Found an organism with a bad reference", organism=organism.name, pvalue=pvalue
+            )
             bad_organisms.append(organism)
+        else:
+            logger.info("Good reference", organism=organism.name, pvalue=pvalue)
 
     if len(bad_organisms) != 0:
         signal_failure(bad_organisms)
@@ -99,20 +95,23 @@ class Command(BaseCommand):
         try:
             check_qn_reference_agreement()
         except Exception as e:
-            requests.post(
-                settings.ENGAGEMENTBOT_WEBHOOK,
-                json={
-                    "attachments": [
-                        {
-                            "fallback": "Exception raised during QN Reference Test",
-                            "title": "Exception raised during QN Reference Test",
-                            "color": "#db3b28",
-                            "text": f"```\n{str(e)}\n```",
-                            "footer": "Refine.bio",
-                            "footer_icon": "https://s3.amazonaws.com/refinebio-email/logo-2x.png",
-                        }
-                    ],
-                },
-                headers={"Content-Type": "application/json"},
-                timeout=10,
-            )
+            if settings.ENGAGEMENTBOT_WEBHOOK:
+                requests.post(
+                    settings.ENGAGEMENTBOT_WEBHOOK,
+                    json={
+                        "attachments": [
+                            {
+                                "fallback": "Exception raised during QN Reference Test",
+                                "title": "Exception raised during QN Reference Test",
+                                "color": "#db3b28",
+                                "text": f"```\n{str(e)}\n```",
+                                "footer": "Refine.bio",
+                                "footer_icon": "https://s3.amazonaws.com/refinebio-email/logo-2x.png",
+                            }
+                        ],
+                    },
+                    headers={"Content-Type": "application/json"},
+                    timeout=10,
+                )
+            else:
+                logger.exception("Exception was raised during QN Reference Test")
