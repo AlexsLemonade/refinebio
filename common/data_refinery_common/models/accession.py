448e070a26678950dc56cd6e748789b4e5790115
Arkadii Yakovets <ark@ccdatalab.org>
2022-09-08 18:12:39 -0700

    Port Python script to Django command.   - Introduce AccessionBacklogEntry model.   - Clean up command flags.   - Get previous accessions from the DB.   -

--- common/data_refinery_common/models/accession.py
+++ common/data_refinery_common/models/accession.py
@@ -1,18 +1,16 @@
+from datetime import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
-class Accession(models.Model):
-    """Accession model."""
+class AccessionBacklogEntry(models.Model):
+    """Accession backlog entry model."""
 
     class Meta:
-        constraints = (
-            models.UniqueConstraint(
-                fields=("code", "source", "technology"), name="unique_accession"
-            ),
-        )
-        db_table = "accessions"
+        db_table = "accession_backlog"
 
-    code = models.TextField()
+    code = models.TextField(unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified_at = models.DateTimeField(auto_now=True)
     organism = models.TextField()
@@ -20,3 +18,67 @@ class Accession(models.Model):
     sample_count = models.PositiveIntegerField(default=0)
     source = models.TextField()
     technology = models.TextField()
+
+    def __eq__(self, other: object) -> bool:
+        """Returns True if two objects are equal. Otherwise returns False."""
+        return isinstance(other, AccessionBacklogEntry) and self.code == other.code
+
+    def __hash__(self) -> int:
+        """Returns accession object unique hash value."""
+        return hash(self.code)
+
+    def __str__(self) -> str:
+        """Returns accession default string representation."""
+        return ", ".join((self.code, self.technology, self.source, str(self.published_date.date())))
+
+    @staticmethod
+    def create_from_ma_ae_entry(entry):
+        """Creates accession object from MicroArray ArrayExpress entry."""
+        accession = AccessionBacklogEntry()
+        accession.code = entry["accession"]
+        accession.source = "ebi_biostudies"
+        accession.technology = "microarray"
+
+        if "organism" in entry:
+            accession.organism = entry["organism"]
+        if "release_date" in entry:
+            accession.published_date = timezone.make_aware(
+                datetime.strptime(entry["release_date"], "%Y-%m-%d")
+            )
+
+        return accession
+
+    @staticmethod
+    def create_from_ma_geo_entry(entry):
+        """Creates accession object from MicroArray GEO meta DB entry."""
+        accession = AccessionBacklogEntry()
+        accession.code = entry["gse"]
+        accession.source = "geo_meta_db"
+        accession.technology = "microarray"
+
+        if "organism" in entry:
+            accession.organism = entry["organism"].lower()
+        if "submission_date" in entry:
+
+            accession.published_date = timezone.make_aware(
+                datetime.strptime(entry["submission_date"], "%Y-%m-%d")
+            )
+
+        return accession
+
+    @staticmethod
+    def create_from_rnaseq_entry(entry):
+        """Creates accession object from RNA-Seq entry."""
+        accession = AccessionBacklogEntry()
+        accession.code = entry["secondary_study_accession"]
+        accession.source = "ebi_ena_portal"
+        accession.technology = "rna-seq"
+
+        if "scientific_name" in entry:
+            accession.organism = entry["scientific_name"].lower()
+        if "first_public" in entry:
+            accession.published_date = timezone.make_aware(
+                datetime.strptime(entry["first_public"], "%Y-%m-%d")
+            )
+
+        return accession
