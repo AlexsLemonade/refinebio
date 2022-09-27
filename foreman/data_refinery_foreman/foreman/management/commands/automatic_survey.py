461ae90d13b6dadf9038511494361f208f97888d
Arkadii Yakovets <ark@ccdatalab.org>
2022-09-27 12:15:22 -0700

    Address PR comments.

--- foreman/data_refinery_foreman/foreman/management/commands/automatic_survey.py
+++ foreman/data_refinery_foreman/foreman/management/commands/automatic_survey.py
@@ -19,7 +19,7 @@ logger = get_and_configure_logger(__name__)
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument("-d", "--days", help="The time window in days.", type=int, default=7)
+        parser.add_argument("-d", "--days", help="The time window in days.", type=int, default=1)
         parser.add_argument(
             "-q",
             "--quota",
@@ -34,39 +34,61 @@ class Command(BaseCommand):
 
 def create_survey_jobs(days, quota):
     """Automatically creates survey jobs based on the passed time window requirements."""
-    surveyed_accessions = (
-        sa["accession_code"] for sa in SurveyedAccession.objects.values("accession_code")
-    )
+
+    remaining_quota = get_remaining_quota(days, quota)
+    if not remaining_quota:
+        return
+
     gathered_accessions = [
         ga["accession_code"]
-        for ga in GatheredAccession.objects.exclude(accession_code__in=surveyed_accessions)
-        .order_by("created_at")
-        .values("accession_code")
+        for ga in GatheredAccession.objects.order_by("created_at").values("accession_code")
     ]
 
+    batch_start = 0
+    batch_size = 1000
+    queued_accessions = list()
+    while True:
+        batch_accessions = gathered_accessions[batch_start : batch_start + batch_size]
+        if not batch_accessions:
+            break
+
+        surveyed_accessions = (
+            sa["accession_code"]
+            for sa in SurveyedAccession.objects.filter(accession_code__in=batch_accessions).values(
+                "accession_code"
+            )
+        )
+
+        for accession_code in set(batch_accessions) - set(surveyed_accessions):
+            try:
+                queue_surveyor_for_accession(accession_code)
+            except Exception as e:
+                logger.error(f"Couldn't queue accession {accession_code} due to: {e}")
+            else:
+                queued_accessions.append(accession_code)
+                if len(queued_accessions) == remaining_quota:
+                    break
+
+        if len(queued_accessions) == remaining_quota:
+            break
+
+        batch_start += batch_size
+
+    queued_accession_count = len(queued_accessions)
+    logger.info(
+        f"Queued {queued_accession_count} accession{pluralize(queued_accession_count)} "
+        f"based on {quota} accession{pluralize(quota)} per {days} "
+        f"day{pluralize(days)} quota."
+    )
+    SurveyedAccession.objects.bulk_create(
+        (SurveyedAccession(accession_code=accession_code) for accession_code in queued_accessions)
+    )
+
+
+def get_remaining_quota(days, quota):
+    """Returns the remaining quota or 0 if nothing left."""
     time_window_survey_jobs = SurveyJob.objects.filter(
         created_at__gt=timezone.now() - relativedelta(days=days)
     )
 
-    remaining_quota = quota - time_window_survey_jobs.count()
-    if remaining_quota > 0:
-        queued_accessions = list()
-        for accession_code in gathered_accessions[:remaining_quota]:
-            try:
-                queue_surveyor_for_accession(accession_code)
-                queued_accessions.append(accession_code)
-            except Exception as e:
-                logger.error(f"Couldn't queue accession {accession_code} due to: {e}")
-
-        queued_accession_count = len(queued_accessions)
-        logger.info(
-            f"Queued {queued_accession_count} accession{pluralize(queued_accession_count)} "
-            f"based on {quota} accession{pluralize(quota)} per {days} "
-            f"day{pluralize(days)} quota."
-        )
-        SurveyedAccession.objects.bulk_create(
-            (
-                SurveyedAccession(accession_code=accession_code)
-                for accession_code in queued_accessions
-            )
-        )
+    return max(0, quota - time_window_survey_jobs.count())
