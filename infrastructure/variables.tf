37c08373a51da26f2121d8ba5b370971660eb5d6
Arkadii Yakovets <ark@ccdatalab.org>
2022-10-06 09:19:40 -0700

    Fix foreman misconfiguration.

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -223,6 +223,14 @@ variable "processing_compendia" {
   default = true
 }
 
+variable "accession_gathering_job_run_day" {
+  default = "SAT"
+}
+
+variable "max_accessions_gathered_per_run" {
+  default = 0
+}
+
 # Output our production environment variables.
 output "environment_variables" {
   value = [
