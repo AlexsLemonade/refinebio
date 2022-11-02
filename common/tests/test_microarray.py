bba6fbee71740aa13e49cbe85e71f016215c5f85
Arkadii Yakovets <ark@ccdatalab.org>
2022-11-02 15:32:16 -0700

    Move all tests to tests directory.
    Add separate settings file (tests/settings.py).
    Refactor import according to the new structure.

---
+++ common/tests/test_microarray.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+
+from data_refinery_common import microarray
+
+
+class MicroarrayTestCase(TestCase):
+    def test_get_platform_from_CEL(self):
+        self.assertEqual(
+            "hgu95av2", microarray.get_platform_from_CEL("test/data/microarray/C30057.CEL.gz")
+        )
+        self.assertEqual(
+            "rgu34a", microarray.get_platform_from_CEL("test/data/microarray/SG2_u34a.CEL.gz")
+        )
+        self.assertEqual(
+            "mouse4302",
+            microarray.get_platform_from_CEL("test/data/microarray/97_(Mouse430_2).CEL.gz"),
+        )
+        self.assertEqual(
+            "zebgene11st", microarray.get_platform_from_CEL("test/data/microarray/CONTROL6.cel.gz")
+        )
