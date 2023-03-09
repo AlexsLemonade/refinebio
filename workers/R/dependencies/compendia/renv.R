75e57c3a3d6842e8c40277ce81ef00bf045086c2
Arkadii Yakovets <ark@ccdatalab.org>
2023-03-09 15:04:25 -0800

    Update compendia Docker image and R dependencies.

---
+++ workers/R/dependencies/compendia/renv.R
@@ -0,0 +1,15 @@
+renv::install("codetools@0.2-15")
+renv::install("iterators@1.0.10")
+renv::install("foreach@1.4.4")
+renv::install("doParallel@1.0.11")
+renv::install("data.table@1.11.0")
+renv::install("getopt@1.20.3")
+renv::install("optparse@1.4.4")
+renv::install("lazyeval@0.2.1")
+renv::install("ff@2.2-14")
+
+renv::install(
+    "oligo",
+    "oligoClasses",
+    "preprocessCore"
+)
