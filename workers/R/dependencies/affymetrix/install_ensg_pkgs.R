531149906d74ee1b448997868c658da004d7115f
Arkadii Yakovets <ark@ccdatalab.org>
2023-03-09 15:18:28 -0800

    Update affymetrix dependencies.

--- workers/R/dependencies/affymetrix/install_ensg_pkgs.R
+++ workers/R/dependencies/affymetrix/install_ensg_pkgs.R
@@ -1,16 +1,17 @@
 options(Ncpus = parallel::detectCores())
-install.packages("xml2")
+
 library("xml2")
+
 ensg_url <- "http://brainarray.mbni.med.umich.edu/Brainarray/Database/CustomCDF/22.0.0/ensg.asp"
 html_content <- read_html(ensg_url)
 
 # The second table in the html is the main table that lists all ensg packages.
 data_table <- xml_find_all(html_content, ".//table")[[2]]
 
-# Extract data rows from the second table:
+# Extract data rows from the second table.
 data_rows <- xml_children(data_table)[3:xml_length(data_table)]
 
-# These two global variables will be modified by save_chip_pkg():
+# These two global variables will be modified by save_chip_pkg().
 chips <- c()
 pkg_urls <- c()
 
@@ -34,15 +35,15 @@ save_chip_pkg <- function(row) {
     }
 }
 
-# Extract data of interest out of each row
+# Extract data of interest out of each row.
 lapply(data_rows, save_chip_pkg)
 
-# Write chips and pkg_urls to a tab-delimited file
+# Write chips and pkg_urls to a tab-delimited file.
 output_filename <- "/home/user/r_ensg_probe_pkgs.txt"
 write.table(list(chips, pkg_urls),
     file = output_filename, quote = FALSE,
     row.names = FALSE, col.names = FALSE, sep = "\t"
 )
 
-# Install these ensg packages
-lapply(pkg_urls, devtools::install_url)
+# Install these ensg packages.
+lapply(pkg_urls, install.packages)
