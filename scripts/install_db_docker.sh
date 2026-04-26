c265afe8776632f47d399a26f0794954ff134054
David Mejia <davidsmejia@gmail.com>
2026-04-26 17:51:07 -0400

    fix install_db_docdker for compose managed drdb

--- scripts/install_db_docker.sh
+++ scripts/install_db_docker.sh
@@ -1,45 +1,13 @@
 #! /bin/sh
 
-POSTGRES_VERSION="9.6.6"
+# Bootstrap the data_refinery database in the running drdb container.
+# Postgres trusts local socket connections, so no PGPASSWORD needed.
 
-docker run \
-    --env PGPASSWORD=mysecretpassword \
-    --link drdb:postgres \
-    --rm \
-    "postgres:$POSTGRES_VERSION" \
-    psql -c "create database data_refinery" -h postgres -U postgres
+set -e
 
-docker run \
-    --env PGPASSWORD=mysecretpassword \
-    --link drdb:postgres \
-    --rm \
-    "postgres:$POSTGRES_VERSION" \
-    psql -c "CREATE ROLE data_refinery_user WITH LOGIN PASSWORD 'data_refinery_password';" -h postgres -U postgres
-
-docker run \
-    --env PGPASSWORD=mysecretpassword \
-    --link drdb:postgres \
-    --rm \
-    "postgres:$POSTGRES_VERSION" \
-    psql -c 'GRANT ALL PRIVILEGES ON DATABASE data_refinery TO data_refinery_user;' -h postgres -U postgres
-
-docker run \
-    --env PGPASSWORD=mysecretpassword \
-    --link drdb:postgres \
-    --rm \
-    "postgres:$POSTGRES_VERSION" \
-    psql -c 'ALTER USER data_refinery_user CREATEDB;' -h postgres -U postgres
-
-docker run \
-    --env PGPASSWORD=mysecretpassword \
-    --link drdb:postgres \
-    --rm \
-    "postgres:$POSTGRES_VERSION" \
-    psql -c 'ALTER ROLE data_refinery_user superuser;' -h postgres -U postgres
-
-docker run \
-    --env PGPASSWORD=mysecretpassword \
-    --link drdb:postgres \
-    --rm \
-    "postgres:$POSTGRES_VERSION" \
-    psql -c 'CREATE EXTENSION IF NOT EXISTS hstore;' -h postgres -U postgres -d data_refinery
+docker exec drdb psql -U postgres -c "CREATE DATABASE data_refinery"
+docker exec drdb psql -U postgres -c "CREATE ROLE data_refinery_user WITH LOGIN PASSWORD 'data_refinery_password'"
+docker exec drdb psql -U postgres -c 'GRANT ALL PRIVILEGES ON DATABASE data_refinery TO data_refinery_user'
+docker exec drdb psql -U postgres -c 'ALTER USER data_refinery_user CREATEDB'
+docker exec drdb psql -U postgres -c 'ALTER ROLE data_refinery_user superuser'
+docker exec drdb psql -U postgres -d data_refinery -c 'CREATE EXTENSION IF NOT EXISTS hstore'
