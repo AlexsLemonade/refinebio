4567ca86489f394f0621c104585edd3eefdc8c76
David Mejia <davidsmejia@gmail.com>
2026-04-27 01:45:08 -0400

    shim db scripts to bin/rbio db:* commands

--- scripts/install_db_docker.sh
+++ scripts/install_db_docker.sh
@@ -1,13 +1,2 @@
-#! /bin/sh
-
-# Bootstrap the data_refinery database in the running drdb container.
-# Postgres trusts local socket connections, so no PGPASSWORD needed.
-
-set -e
-
-docker exec drdb psql -U postgres -c "CREATE DATABASE data_refinery"
-docker exec drdb psql -U postgres -c "CREATE ROLE data_refinery_user WITH LOGIN PASSWORD 'data_refinery_password'"
-docker exec drdb psql -U postgres -c 'GRANT ALL PRIVILEGES ON DATABASE data_refinery TO data_refinery_user'
-docker exec drdb psql -U postgres -c 'ALTER USER data_refinery_user CREATEDB'
-docker exec drdb psql -U postgres -c 'ALTER ROLE data_refinery_user superuser'
-docker exec drdb psql -U postgres -d data_refinery -c 'CREATE EXTENSION IF NOT EXISTS hstore'
+#!/bin/sh
+exec "$(dirname "$0")/../bin/rbio" db:init "$@"
